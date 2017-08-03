8dd747f1bb6d136fb0dbac0bd4156b575d635a18
Alexis Tan <alexis@sen-dev.com>
2017-08-03 14:16:46 +0200

    Make Orion connect to Discord

--- src/main/java/com/avairebot/orion/Orion.java
+++ src/main/java/com/avairebot/orion/Orion.java
@@ -2,10 +2,16 @@ package com.avairebot.orion;
 
 import com.avairebot.orion.config.ConfigurationLoader;
 import com.avairebot.orion.config.MainConfiguration;
+import net.dv8tion.jda.core.AccountType;
+import net.dv8tion.jda.core.JDABuilder;
+import net.dv8tion.jda.core.events.Event;
+import net.dv8tion.jda.core.exceptions.RateLimitedException;
+import net.dv8tion.jda.core.hooks.EventListener;
 
+import javax.security.auth.login.LoginException;
 import java.io.IOException;
 
-public class Orion {
+public class Orion implements EventListener {
 
     public final MainConfiguration config;
 
@@ -13,8 +19,21 @@ public class Orion {
         ConfigurationLoader configLoader = new ConfigurationLoader();
         this.config = (MainConfiguration) configLoader.load("config.json", MainConfiguration.class);
         if (this.config == null) {
-            System.out.println("Something went wrong while trying to load the configuration, exiting program...");
+            System.err.println("Something went wrong while trying to load the configuration, exiting program...");
             System.exit(0);
         }
+
+        try {
+            new JDABuilder(AccountType.BOT).setToken(this.config.botAuth().getToken()).addEventListener(this).buildAsync();
+        } catch (LoginException | RateLimitedException e) {
+            System.err.println("Something went wrong while trying to connect to Discord, exiting program...");
+            e.printStackTrace();
+            System.exit(0);
+        }
+    }
+
+    @Override
+    public void onEvent(Event e) {
+        System.out.println(e.getClass().getName());
     }
 }
