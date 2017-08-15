792dd548daa95a693f9f65e13e7f78fcd4711c60
Alexis Tan <alexis@sen-dev.com>
2017-08-15 19:45:08 +0200

    Create message factory method with support for fields

--- src/main/java/com/avairebot/orion/factories/MessageFactory.java
+++ src/main/java/com/avairebot/orion/factories/MessageFactory.java
@@ -3,9 +3,11 @@ package com.avairebot.orion.factories;
 import net.dv8tion.jda.core.EmbedBuilder;
 import net.dv8tion.jda.core.entities.Message;
 import net.dv8tion.jda.core.entities.MessageChannel;
+import net.dv8tion.jda.core.entities.MessageEmbed.Field;
 import net.dv8tion.jda.core.requests.RestAction;
 
 import java.awt.*;
+import java.util.Arrays;
 
 import static com.avairebot.orion.factories.MessageFactory.MessageType.*;
 
@@ -33,6 +35,12 @@ public class MessageFactory {
 
     public static RestAction<Message> makeEmbeddedMessage(MessageChannel channel, Color color, String message) {
         return channel.sendMessage(new EmbedBuilder().setColor(color).setDescription(message).build());
+    }
+
+    public static RestAction<Message> makeEmbeddedMessage(MessageChannel channel, MessageType type, Field... fields) {
+        EmbedBuilder embed = new EmbedBuilder().setColor(type.getColor());
+        Arrays.stream(fields).forEachOrdered(embed::addField);
+        return channel.sendMessage(embed.build());
     }
 
     private static String prepareMessage(Message jdaMessage, String message, Object... args) {
