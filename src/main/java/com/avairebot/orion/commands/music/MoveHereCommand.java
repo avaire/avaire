fe65897d56e443238a7f0be7a438940efbb54e00
Alexis Tan <alexis@sen-dev.com>
2017-09-18 18:35:07 +0200

    Refactor music move here command to use audio handle connect method

--- src/main/java/com/avairebot/orion/commands/music/MoveHereCommand.java
+++ src/main/java/com/avairebot/orion/commands/music/MoveHereCommand.java
@@ -3,12 +3,11 @@ package com.avairebot.orion.commands.music;
 import com.avairebot.orion.Orion;
 import com.avairebot.orion.audio.AudioHandler;
 import com.avairebot.orion.audio.GuildMusicManager;
+import com.avairebot.orion.audio.VoiceConnectStatus;
 import com.avairebot.orion.contracts.commands.AbstractCommand;
 import com.avairebot.orion.factories.MessageFactory;
-import net.dv8tion.jda.core.Permission;
 import net.dv8tion.jda.core.entities.Message;
 import net.dv8tion.jda.core.entities.VoiceChannel;
-import net.dv8tion.jda.core.exceptions.InsufficientPermissionException;
 
 import java.util.Arrays;
 import java.util.Collections;
@@ -58,26 +57,17 @@ public class MoveHereCommand extends AbstractCommand {
             return sendErrorMessage(message, "You must be connected to a voice channel to use this command!");
         }
 
-        List<Permission> permissions = message.getGuild().getMember(message.getJDA().getSelfUser()).getPermissions(
-                message.getMember().getVoiceState().getChannel()
-        );
+        VoiceConnectStatus voiceConnectStatus = AudioHandler.connectToVoiceChannel(message, true);
 
-        if (!permissions.contains(Permission.VOICE_CONNECT)) {
-            MessageFactory.makeWarning(message, "Unable to connect to the voice channel you're in due to missing permissions!").queue();
+        if (!voiceConnectStatus.isSuccess()) {
+            MessageFactory.makeWarning(message, voiceConnectStatus.getErrorMessage()).queue();
             return false;
         }
 
-        try {
-            message.getGuild().getAudioManager().openAudioConnection(channel);
-            MessageFactory.makeSuccess(message, "I am now streaming music in **%s**",
-                    channel.getName()
-            ).queue();
-        } catch (InsufficientPermissionException ex) {
-            MessageFactory.makeWarning(message, "Unable to connect to the voice channel you're in due to userlimit!").queue();
-            orion.logger.exception(ex);
-            return false;
-        }
-
+        MessageFactory.makeSuccess(message, "I am now streaming music in **%s**",
+                channel.getName()
+        ).queue();
         return true;
+
     }
 }
