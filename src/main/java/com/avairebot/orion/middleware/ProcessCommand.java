24ccf6d897f8686aaacbb47a78970d8395cb4a26
Alexis Tan <alexis@sen-dev.com>
2017-09-23 05:04:24 +0200

    Modify process command log message to contain more information

--- src/main/java/com/avairebot/orion/middleware/ProcessCommand.java
+++ src/main/java/com/avairebot/orion/middleware/ProcessCommand.java
@@ -3,12 +3,17 @@ package com.avairebot.orion.middleware;
 import com.avairebot.orion.Orion;
 import com.avairebot.orion.contracts.middleware.AbstractMiddleware;
 import net.dv8tion.jda.core.entities.Message;
-import net.dv8tion.jda.core.entities.User;
 
 import java.util.Arrays;
 import java.util.regex.Pattern;
 
 public class ProcessCommand extends AbstractMiddleware {
+
+    private final static String COMMAND_OUTPUT = "Executing Command \"%command%\" in \"%category%\" category:"
+            + "\n\t\tUser:\t %author%"
+            + "\n\t\tServer:\t %server%"
+            + "\n\t\tChannel: %channel%"
+            + "\n\t\tMessage: %message%";
 
     private final Pattern argumentsRegEX;
 
@@ -22,13 +27,45 @@ public class ProcessCommand extends AbstractMiddleware {
     public boolean handle(Message message, MiddlewareStack stack, String... args) {
         String[] arguments = argumentsRegEX.split(message.getContent());
 
-        User author = message.getAuthor();
-        orion.logger.info(String.format("Executing Command <%s> from %s#%s",
-                message.getContent(),
-                author.getName(),
-                author.getDiscriminator()
-        ));
+        orion.logger.info(COMMAND_OUTPUT
+                .replace("%command%", stack.getCommand().getName())
+                .replace("%category%", stack.getCommandContainer().getCategory().getName())
+                .replace("%author%", generateUsername(message))
+                .replace("%server%", generateServer(message))
+                .replace("%channel%", generateChannel(message))
+                .replace("%message%", message.getRawContent())
+        );
 
         return stack.getCommand().onCommand(message, Arrays.copyOfRange(arguments, 1, arguments.length));
+    }
+
+    private String generateUsername(Message message) {
+        return String.format("%s#%s [%s]",
+                message.getAuthor().getName(),
+                message.getAuthor().getDiscriminator(),
+                message.getAuthor().getId()
+        );
+    }
+
+    private String generateServer(Message message) {
+        if (message.getChannelType().isGuild()) {
+            return "PRIVATE";
+        }
+
+        return String.format("%s [%s]",
+                message.getGuild().getName(),
+                message.getGuild().getId()
+        );
+    }
+
+    private CharSequence generateChannel(Message message) {
+        if (message.getChannelType().isGuild()) {
+            return "PRIVATE";
+        }
+
+        return String.format("%s [%s]",
+                message.getChannel().getName(),
+                message.getChannel().getId()
+        );
     }
 }
