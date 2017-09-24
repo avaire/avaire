4c0375957a2c74297a374e37b23b885af914fc1b
Alexis Tan <alexis@sen-dev.com>
2017-09-24 16:17:09 +0200

    Move database guild fetching into guild data controller

---
+++ src/main/java/com/avairebot/orion/database/controllers/GuildController.java
@@ -0,0 +1,89 @@
+package com.avairebot.orion.database.controllers;
+
+import com.avairebot.orion.Constants;
+import com.avairebot.orion.Orion;
+import com.avairebot.orion.cache.CacheType;
+import com.avairebot.orion.database.collection.DataRow;
+import com.avairebot.orion.database.transformers.GuildTransformer;
+import com.google.gson.Gson;
+import net.dv8tion.jda.core.entities.Message;
+import net.dv8tion.jda.core.entities.TextChannel;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class GuildController {
+
+    private static final String CACHE_STRING = "database.guilds.%s";
+
+    public static GuildTransformer fetchGuild(Orion orion, Message message) {
+        if (!message.getChannelType().isGuild()) {
+            return null;
+        }
+
+        if (isCached(orion, message.getGuild().getId())) {
+            return (GuildTransformer) orion.cache.getAdapter(CacheType.MEMORY).get(
+                    String.format(CACHE_STRING, message.getGuild().getId())
+            );
+        }
+
+        try {
+            GuildTransformer transformer = new GuildTransformer(orion.database.newQueryBuilder(Constants.GUILD_TABLE_NAME)
+                    .selectAll()
+                    .where("id", message.getGuild().getId())
+                    .get().first());
+
+            if (!transformer.hasData()) {
+                Map<String, Object> items = new HashMap<>();
+                items.put("id", message.getGuild().getId());
+                items.put("owner", message.getGuild().getOwner().getUser().getId());
+                items.put("name", message.getGuild().getName());
+                items.put("channels_data", buildChannelData(message.getGuild().getTextChannels()));
+                if (message.getGuild().getIconId() != null) {
+                    items.put("icon", message.getGuild().getIconId());
+                }
+
+                transformer = new GuildTransformer(new DataRow(items));
+                orion.cache.getAdapter(CacheType.MEMORY).put(String.format(CACHE_STRING, message.getGuild().getId()), transformer, 2);
+
+                try {
+                    orion.database.newQueryBuilder(Constants.GUILD_TABLE_NAME).insert(items);
+                } catch (Exception ex) {
+                    orion.logger.fatal(ex);
+                }
+
+                return transformer;
+            }
+
+            orion.cache.getAdapter(CacheType.MEMORY).put(String.format(CACHE_STRING, message.getGuild().getId()), transformer, 300);
+
+            return transformer;
+        } catch (SQLException ex) {
+            orion.logger.fatal(ex);
+            return null;
+        }
+    }
+
+    private static boolean isCached(Orion orion, String guildId) {
+        return orion.cache.getAdapter(CacheType.MEMORY).has(
+                String.format(CACHE_STRING, guildId)
+        );
+    }
+
+    private static String buildChannelData(List<TextChannel> textChannels) {
+        List<Map<String, Object>> channels = new ArrayList<>();
+        for (TextChannel channel : textChannels) {
+            Map<String, Object> item = new HashMap<>();
+
+            item.put("id", channel.getId());
+            item.put("name", channel.getName());
+            item.put("position", channel.getPosition());
+
+            channels.add(item);
+        }
+        return new Gson().toJson(channels);
+    }
+}
--- src/main/java/com/avairebot/orion/handlers/MessageCreateEvent.java
+++ src/main/java/com/avairebot/orion/handlers/MessageCreateEvent.java
@@ -1,25 +1,16 @@
 package com.avairebot.orion.handlers;
 
-import com.avairebot.orion.Constants;
 import com.avairebot.orion.Orion;
 import com.avairebot.orion.Statistics;
-import com.avairebot.orion.cache.CacheType;
 import com.avairebot.orion.commands.CommandContainer;
 import com.avairebot.orion.commands.CommandHandler;
 import com.avairebot.orion.contracts.handlers.EventHandler;
-import com.avairebot.orion.database.collection.DataRow;
+import com.avairebot.orion.database.controllers.GuildController;
 import com.avairebot.orion.database.transformers.GuildTransformer;
 import com.avairebot.orion.factories.MessageFactory;
 import com.avairebot.orion.middleware.MiddlewareStack;
-import com.google.gson.Gson;
-import net.dv8tion.jda.core.entities.TextChannel;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 
-import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 
 public class MessageCreateEvent extends EventHandler {
@@ -52,64 +43,6 @@ public class MessageCreateEvent extends EventHandler {
     }
 
     private CompletableFuture<GuildTransformer> loadDatabasePropertiesIntoMemory(final MessageReceivedEvent event) {
-        return CompletableFuture.supplyAsync(() -> {
-            if (!event.getChannelType().isGuild()) {
-                return null;
-            }
-
-            if (orion.cache.getAdapter(CacheType.MEMORY).has("guilds." + event.getGuild().getId())) {
-                return (GuildTransformer) orion.cache.getAdapter(CacheType.MEMORY).get("guilds." + event.getGuild().getId());
-            }
-
-            try {
-                GuildTransformer transformer = new GuildTransformer(orion.database.newQueryBuilder(Constants.GUILD_TABLE_NAME)
-                        .selectAll()
-                        .where("id", event.getGuild().getId())
-                        .get().first());
-
-                if (!transformer.hasData()) {
-                    Map<String, Object> items = new HashMap<>();
-                    items.put("id", event.getGuild().getId());
-                    items.put("owner", event.getGuild().getOwner().getUser().getId());
-                    items.put("name", event.getGuild().getName());
-                    items.put("channels_data", buildChannelData(event.getGuild().getTextChannels()));
-                    if (event.getGuild().getIconId() != null) {
-                        items.put("icon", event.getGuild().getIconId());
-                    }
-
-                    transformer = new GuildTransformer(new DataRow(items));
-                    orion.cache.getAdapter(CacheType.MEMORY).put("guilds." + event.getGuild().getId(), transformer, 2);
-
-                    try {
-                        orion.database.newQueryBuilder(Constants.GUILD_TABLE_NAME).insert(items);
-                    } catch (Exception ex) {
-                        orion.logger.fatal(ex);
-                    }
-
-                    return transformer;
-                }
-
-                orion.cache.getAdapter(CacheType.MEMORY).put("guilds." + event.getGuild().getId(), transformer, 300);
-
-                return transformer;
-            } catch (SQLException ex) {
-                orion.logger.fatal(ex);
-                return null;
-            }
-        });
-    }
-
-    private String buildChannelData(List<TextChannel> textChannels) {
-        List<Map<String, Object>> channels = new ArrayList<>();
-        for (TextChannel channel : textChannels) {
-            Map<String, Object> item = new HashMap<>();
-
-            item.put("id", channel.getId());
-            item.put("name", channel.getName());
-            item.put("position", channel.getPosition());
-
-            channels.add(item);
-        }
-        return new Gson().toJson(channels);
+        return CompletableFuture.supplyAsync(() -> GuildController.fetchGuild(orion, event.getMessage()));
     }
 }
