d39b4c591d343e1f722b8ea25c0b0c06f46906e5
Alexis Tan <alexis@sen-dev.com>
2017-10-15 18:20:57 +0200

    Create voice kick command

--- src/main/java/com/avairebot/orion/Orion.java
+++ src/main/java/com/avairebot/orion/Orion.java
@@ -112,6 +112,7 @@ public class Orion {
         CommandHandler.register(new ServerIdCommand(this));
         CommandHandler.register(new SoftBanCommand(this));
         CommandHandler.register(new UserIdCommand(this));
+        CommandHandler.register(new VoiceKickCommand(this));
         CommandHandler.register(new WelcomeCommand(this));
         CommandHandler.register(new WelcomeMessageCommand(this));
 
