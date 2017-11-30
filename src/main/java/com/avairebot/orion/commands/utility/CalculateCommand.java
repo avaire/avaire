1bf17de93ab1d294904ff6cc854e596b524fde08
Alexis Tan <alexis@sen-dev.com>
2017-11-30 17:40:16 +0100

    Create calculate command

---
+++ src/main/java/com/avairebot/orion/commands/utility/CalculateCommand.java
@@ -0,0 +1,84 @@
+package com.avairebot.orion.commands.utility;
+
+import com.avairebot.orion.Orion;
+import com.avairebot.orion.contracts.commands.Command;
+import com.avairebot.orion.factories.MessageFactory;
+import com.udojava.evalex.Expression;
+import net.dv8tion.jda.core.entities.Message;
+
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.regex.Pattern;
+
+public class CalculateCommand extends Command {
+
+    private static final Pattern NUMBER_REGEX = Pattern.compile("^\\d+$");
+
+    public CalculateCommand(Orion orion) {
+        super(orion);
+    }
+
+    @Override
+    public String getName() {
+        return "Calculate Command";
+    }
+
+    @Override
+    public String getDescription() {
+        return "Calculates the given math equations and returns the result for you.";
+    }
+
+    @Override
+    public List<String> getUsageInstructions() {
+        return Collections.singletonList("`:command <equation>` - Calculates the result of the given math equation.");
+    }
+
+    @Override
+    public String getExampleUsage() {
+        return "`:command (-50 + sqrt(50 ^ 2 - ((4 * 5) * (100 - 955)))) / (2 * 5) == 9`";
+    }
+
+    @Override
+    public List<String> getTriggers() {
+        return Arrays.asList("calculate", "calc");
+    }
+
+    @Override
+    public boolean onCommand(Message message, String[] args) {
+        if (args.length == 0) {
+            return sendErrorMessage(message, "Missing argument, the `equation` argument is required!");
+        }
+
+        String string = String.join(" ", args).trim();
+        if (NUMBER_REGEX.matcher(string).find()) {
+            MessageFactory.makeInfo(message, string).queue();
+            return true;
+        }
+
+        try {
+            Expression expression = createExpression(string);
+            BigDecimal result = expression.eval();
+
+            System.out.println(expression.getExpression());
+            if (expression.isBoolean()) {
+
+                MessageFactory.makeInfo(message, result.intValueExact() == 1
+                    ? "True" : "False"
+                ).queue();
+                return true;
+            }
+
+            MessageFactory.makeInfo(message, result.toPlainString()).queue();
+        } catch (Exception ex) {
+            return sendErrorMessage(message, ex.getMessage().replaceAll("'", "`"));
+        }
+        return true;
+    }
+
+    private Expression createExpression(String string) {
+        return new Expression(string)
+            .setVariable("tau", new BigDecimal(Math.PI * 2));
+    }
+}
