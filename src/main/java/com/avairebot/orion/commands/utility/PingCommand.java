05acafdb83c0c24c9b05ad4a5c593387502e0e59
Alexis Tan <alexis@sen-dev.com>
2017-12-07 16:46:00 +0100

    Add message ping and rating to the ping command

--- src/main/java/com/avairebot/orion/commands/utility/PingCommand.java
+++ src/main/java/com/avairebot/orion/commands/utility/PingCommand.java
@@ -3,6 +3,7 @@ package com.avairebot.orion.commands.utility;
 import com.avairebot.orion.Orion;
 import com.avairebot.orion.contracts.commands.Command;
 import com.avairebot.orion.factories.MessageFactory;
+import com.avairebot.orion.utilities.NumberUtil;
 import net.dv8tion.jda.core.entities.Message;
 
 import java.util.Collections;
@@ -41,9 +42,33 @@ public class PingCommand extends Command {
 
     @Override
     public boolean onCommand(Message message, String[] args) {
-        MessageFactory.makeInfo(message, "Pong! Time taken :heartbeat ms!")
-            .set("heartbeat", message.getJDA().getPing())
-            .queue();
+        long start = System.currentTimeMillis();
+        message.getChannel().sendTyping().queue(v -> {
+            long ping = System.currentTimeMillis() - start;
+
+            MessageFactory.makeInfo(message, "Pong! Time taken :ping ms (:rating) Websocket heartbeat :heartbeat ms")
+                .set("heartbeat", message.getJDA().getPing())
+                .set("rating", ratePing(ping))
+                .set("ping", ping)
+                .queue();
+
+        });
         return true;
+    }
+
+    private String ratePing(long ping) {
+        if (ping <= 10) return "faster than Sonic! :smiley_cat:";
+        if (ping <= 100) return "great! :smiley:";
+        if (ping <= 200) return "nice! :slight_smile:";
+        if (ping <= 300) return "decent. :neutral_face:";
+        if (ping <= 400) return "average... :confused:";
+        if (ping <= 500) return "slightly slow. :slight_frown:";
+        if (ping <= 600) return "kinda slow.. :frowning2:";
+        if (ping <= 700) return "slow.. :worried:";
+        if (ping <= 800) return "too slow. :disappointed:";
+        if (ping <= 900) return "bad. :sob: (helpme)";
+        if (ping <= 1600) return "#BlameDiscord. :angry:";
+        if (ping <= 10000) return "this makes no sense :thinking: #BlameAlexis";
+        return "slow af. :dizzy_face: ";
     }
 }
