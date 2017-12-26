f974a48537cb776cb87d7c45edc4aa0ef4dd04cf
Alexis Tan <alexis@sen-dev.com>
2017-12-26 17:57:24 +0100

    Add audio sessions cleanup to garbage collector job

--- src/main/java/com/avairebot/orion/scheduler/GarbageCollectorJob.java
+++ src/main/java/com/avairebot/orion/scheduler/GarbageCollectorJob.java
@@ -38,6 +38,10 @@ public class GarbageCollectorJob extends Job {
 
             AudioHandler.MUSIC_MANAGER.remove(entry.getKey());
         }
+
+        // 3 minutes
+        AudioHandler.AUDIO_SESSION.entrySet()
+            .removeIf(next -> (next.getValue().getCreatedAt() + 180000) < System.currentTimeMillis());
     }
 
     private boolean isConnected(AudioManager audioManager) {
