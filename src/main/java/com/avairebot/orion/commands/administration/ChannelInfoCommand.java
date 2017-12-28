002b2d31f508bf874ad487f5afb15ece1295857f
Alexis Tan <alexis@sen-dev.com>
2017-12-28 22:11:28 +0100

    Add support for voice channels in channel info command

--- src/main/java/com/avairebot/orion/commands/administration/ChannelInfoCommand.java
+++ src/main/java/com/avairebot/orion/commands/administration/ChannelInfoCommand.java
@@ -1,11 +1,16 @@
 package com.avairebot.orion.commands.administration;
 
 import com.avairebot.orion.Orion;
+import com.avairebot.orion.chat.MessageType;
+import com.avairebot.orion.chat.PlaceholderMessage;
 import com.avairebot.orion.contracts.commands.Command;
 import com.avairebot.orion.factories.MessageFactory;
 import com.avairebot.orion.time.Carbon;
+import com.avairebot.orion.utilities.MentionableUtil;
+import net.dv8tion.jda.core.entities.Channel;
 import net.dv8tion.jda.core.entities.Message;
 import net.dv8tion.jda.core.entities.TextChannel;
+import net.dv8tion.jda.core.entities.VoiceChannel;
 
 import java.util.Arrays;
 import java.util.List;
@@ -43,32 +48,55 @@ public class ChannelInfoCommand extends Command {
 
     @Override
     public boolean onCommand(Message message, String[] args) {
-        TextChannel channel = message.getTextChannel();
-        if (message.getMentionedChannels().isEmpty()) {
-            channel = message.getTextChannel();
+        Channel channel = message.getTextChannel();
+        if (args.length > 0) {
+            channel = MentionableUtil.getChannel(message, args);
+
+            if (channel == null) {
+                return sendErrorMessage(message, "I found no channels with the name or ID of `%s`", args[0]);
+            }
         }
 
         Carbon time = Carbon.createFromOffsetDateTime(channel.getCreationTime());
 
-        String topic = "*No topic has been set for this channel*";
-        if (channel.getTopic() != null) {
-            topic = channel.getTopic();
-        }
-
-        MessageFactory.makeInfo(message, topic)
+        PlaceholderMessage placeholder = MessageFactory.makeEmbeddedMessage(message.getChannel())
+            .setColor(MessageType.INFO.getColor())
             .setTitle("#" + channel.getName())
             .addField("ID", channel.getId(), true)
             .addField("Position", "" + channel.getPosition(), true)
             .addField("Users", "" + channel.getMembers().size(), true)
-            .addField("Category", getCategoryFor(channel), true)
-            .addField("NSFW", channel.isNSFW() ? "ON" : "OFF", true)
+            .addField("Category", getCategoryFor(channel), true);
+
+        if (channel instanceof TextChannel) {
+            TextChannel textChannel = (TextChannel) channel;
+
+            String topic = "*No topic has been set for this channel*";
+            if (textChannel.getTopic() != null && textChannel.getTopic().trim().length() > 0) {
+                topic = textChannel.getTopic();
+            }
+
+            placeholder
+                .setDescription(topic)
+                .addField("NSFW", textChannel.isNSFW() ? "ON" : "OFF", true);
+        }
+
+        if (channel instanceof VoiceChannel) {
+            VoiceChannel voiceChannel = (VoiceChannel) channel;
+            int bitRate = voiceChannel.getBitrate() / 1000;
+
+            placeholder
+                .setDescription("*This voice channel has *")
+                .addField("Bit Rate", bitRate + " kbps", true);
+        }
+
+        placeholder
             .addField("Created At", time.format("EEE, dd MMM yyyy HH:mm") + "\n*About " + shortenDiffForHumans(time) + "*", true)
             .queue();
 
         return true;
     }
 
-    private String getCategoryFor(TextChannel channel) {
+    private String getCategoryFor(Channel channel) {
         if (channel.getParent() == null) {
             return "*No Category*";
         }
