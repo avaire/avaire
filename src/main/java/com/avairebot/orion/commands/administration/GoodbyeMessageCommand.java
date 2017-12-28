b83efd03390da3f186590cc82cd22c75b441be0a
Alexis Tan <alexis@sen-dev.com>
2017-12-28 18:32:48 +0100

    Add support for example messages in welcome and goodbye messages

--- src/main/java/com/avairebot/orion/commands/administration/GoodbyeMessageCommand.java
+++ src/main/java/com/avairebot/orion/commands/administration/GoodbyeMessageCommand.java
@@ -8,7 +8,10 @@ import com.avairebot.orion.database.controllers.GuildController;
 import com.avairebot.orion.database.transformers.ChannelTransformer;
 import com.avairebot.orion.database.transformers.GuildTransformer;
 import com.avairebot.orion.factories.MessageFactory;
+import com.avairebot.orion.utilities.MentionableUtil;
+import com.avairebot.orion.utilities.StringReplacementUtil;
 import net.dv8tion.jda.core.entities.Message;
+import net.dv8tion.jda.core.entities.User;
 
 import java.sql.SQLException;
 import java.util.Arrays;
@@ -35,7 +38,8 @@ public class GoodbyeMessageCommand extends Command {
     public List<String> getUsageInstructions() {
         return Arrays.asList(
             "`:command` - Resets the goodbye back to the default message.",
-            "`:command <message>` - Sets the goodbye message to the given message."
+            "`:command <message>` - Sets the goodbye message to the given message.",
+            "`:command <user>` - If a valid username, nickname or user was mentioned, an example message will be sent for the given user."
         );
     }
 
@@ -57,7 +61,6 @@ public class GoodbyeMessageCommand extends Command {
         );
     }
 
-
     @Override
     public boolean onCommand(Message message, String[] args) {
         GuildTransformer guildTransformer = GuildController.fetchGuild(orion, message);
@@ -67,6 +70,14 @@ public class GoodbyeMessageCommand extends Command {
             return sendErrorMessage(message, "The `goodbye` module must be enabled to use this command, you can enable the `goodbye` module by using the `.goodbye` command.");
         }
 
+        if (args.length == 1) {
+            User user = MentionableUtil.getUser(message, args, 0);
+
+            if (user != null) {
+                return sendExampleMessage(message, user, channelTransformer);
+            }
+        }
+
         channelTransformer.getGoodbye().setMessage(args.length == 0 ? null : String.join(" ", args));
 
         try {
@@ -74,11 +85,13 @@ public class GoodbyeMessageCommand extends Command {
                 .andWhere("id", message.getGuild().getId())
                 .update(statement -> statement.set("channels", guildTransformer.channelsToJson(), true));
 
-            MessageFactory.makeSuccess(message, "The `Goodbye` module message has been set :message")
-                .set("message", channelTransformer.getGoodbye().getMessage() != null ?
-                    "to:\n\n`" + channelTransformer.getGoodbye().getMessage() + "`" :
-                    "back to the default."
-                ).queue();
+            if (channelTransformer.getGoodbye().getMessage() == null) {
+                MessageFactory.makeSuccess(message, "The `Goodbye` module message has been set back to the default.").queue();
+
+                return true;
+            }
+
+            return sendEnableMessage(message, channelTransformer);
         } catch (SQLException ex) {
             Orion.getLogger().error("Failed to update the goodbye message", ex);
 
@@ -87,6 +100,37 @@ public class GoodbyeMessageCommand extends Command {
                 .queue();
             return false;
         }
+    }
+
+    private boolean sendExampleMessage(Message message, User user, ChannelTransformer transformer) {
+        message.getChannel().sendMessage(
+            StringReplacementUtil.parseChannel(message.getTextChannel(),
+                StringReplacementUtil.parseUser(user,
+                    StringReplacementUtil.parseGuild(message.getGuild(),
+                        transformer.getGoodbye().getMessage() == null ?
+                            "%user% has left **%server%**! :(" :
+                            transformer.getGoodbye().getMessage()
+                    )
+                )
+            )
+        ).queue();
+
+        return true;
+    }
+
+    private boolean sendEnableMessage(Message message, ChannelTransformer channelTransformer) {
+        MessageFactory.makeSuccess(message, String.join("\n",
+            "The `Welcome` module message has been set to:",
+            "",
+            "```:message```",
+            "",
+            "You can test the message by using the command again and mentioning a user.",
+            "`:command <user>`"
+        ))
+            .set("message", channelTransformer.getWelcome().getMessage())
+            .set("command", generateCommandTrigger(message))
+            .queue();
+
         return true;
     }
 }
--- src/main/java/com/avairebot/orion/commands/administration/WelcomeMessageCommand.java
+++ src/main/java/com/avairebot/orion/commands/administration/WelcomeMessageCommand.java
@@ -8,7 +8,10 @@ import com.avairebot.orion.database.controllers.GuildController;
 import com.avairebot.orion.database.transformers.ChannelTransformer;
 import com.avairebot.orion.database.transformers.GuildTransformer;
 import com.avairebot.orion.factories.MessageFactory;
+import com.avairebot.orion.utilities.MentionableUtil;
+import com.avairebot.orion.utilities.StringReplacementUtil;
 import net.dv8tion.jda.core.entities.Message;
+import net.dv8tion.jda.core.entities.User;
 
 import java.sql.SQLException;
 import java.util.Arrays;
@@ -35,7 +38,8 @@ public class WelcomeMessageCommand extends Command {
     public List<String> getUsageInstructions() {
         return Arrays.asList(
             "`:command` - Resets the welcome back to the default message.",
-            "`:command <message>` - Sets the welcome message to the given message."
+            "`:command <message>` - Sets the welcome message to the given message.",
+            "`:command <user>` - If a valid username, nickname or user was mentioned, an example message will be sent for the given user."
         );
     }
 
@@ -66,6 +70,14 @@ public class WelcomeMessageCommand extends Command {
             return sendErrorMessage(message, "The `welcome` module must be enabled to use this command, you can enable the `welcome` module by using the `.welcome` command.");
         }
 
+        if (args.length == 1) {
+            User user = MentionableUtil.getUser(message, args, 0);
+
+            if (user != null) {
+                return sendExampleMessage(message, user, channelTransformer);
+            }
+        }
+
         channelTransformer.getWelcome().setMessage(args.length == 0 ? null : String.join(" ", args));
 
         try {
@@ -73,17 +85,50 @@ public class WelcomeMessageCommand extends Command {
                 .andWhere("id", message.getGuild().getId())
                 .update(statement -> statement.set("channels", guildTransformer.channelsToJson(), true));
 
-            MessageFactory.makeSuccess(message, "The `Welcome` module message has been set :note")
-                .set("note", channelTransformer.getWelcome().getMessage() != null ?
-                    "to:\n\n`" + channelTransformer.getWelcome().getMessage() + "`" :
-                    "back to the default."
-                ).queue();
+            if (channelTransformer.getWelcome().getMessage() == null) {
+                MessageFactory.makeSuccess(message, "The `Welcome` module message has been set back to the default.").queue();
+
+                return true;
+            }
+
+            return sendEnableMessage(message, channelTransformer);
         } catch (SQLException ex) {
             Orion.getLogger().error(ex.getMessage(), ex);
 
             MessageFactory.makeError(message, "Failed to save the guild settings: " + ex.getMessage()).queue();
             return false;
         }
+    }
+
+    private boolean sendExampleMessage(Message message, User user, ChannelTransformer transformer) {
+        message.getChannel().sendMessage(
+            StringReplacementUtil.parseChannel(message.getTextChannel(),
+                StringReplacementUtil.parseUser(user,
+                    StringReplacementUtil.parseGuild(message.getGuild(),
+                        transformer.getWelcome().getMessage() == null ?
+                            "Welcome %user% to **%server%!**" :
+                            transformer.getWelcome().getMessage()
+                    )
+                )
+            )
+        ).queue();
+
+        return true;
+    }
+
+    private boolean sendEnableMessage(Message message, ChannelTransformer channelTransformer) {
+        MessageFactory.makeSuccess(message, String.join("\n",
+            "The `Welcome` module message has been set to:",
+            "",
+            "```:message```",
+            "",
+            "You can test the message by using the command again and mentioning a user.",
+            "`:command <user>`"
+        ))
+            .set("message", channelTransformer.getWelcome().getMessage())
+            .set("command", generateCommandTrigger(message))
+            .queue();
+
         return true;
     }
 }
