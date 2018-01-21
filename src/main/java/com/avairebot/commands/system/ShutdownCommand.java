a7fc84e98230dd57a13295188a1df4c9ea4be333
Alexis Tan <alexis@sen-dev.com>
2018-01-21 05:36:33 +0100

    Move shutdown command loginc into an abstract class

--- src/main/java/com/avairebot/commands/system/ShutdownCommand.java
+++ src/main/java/com/avairebot/commands/system/ShutdownCommand.java
@@ -1,20 +1,14 @@
 package com.avairebot.commands.system;
 
 import com.avairebot.AvaIre;
-import com.avairebot.commands.CommandMessage;
-import com.avairebot.contracts.commands.SystemCommand;
-import com.avairebot.factories.MessageFactory;
+import com.avairebot.contracts.commands.ApplicationShutdownCommand;
 import com.avairebot.shared.ExitCodes;
-import com.avairebot.time.Carbon;
-import com.avairebot.time.Formats;
-import net.dv8tion.jda.core.entities.Message;
 
-import java.text.ParseException;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-public class ShutdownCommand extends SystemCommand {
+public class ShutdownCommand extends ApplicationShutdownCommand {
 
     public ShutdownCommand(AvaIre avaire) {
         super(avaire);
@@ -45,65 +39,22 @@ public class ShutdownCommand extends SystemCommand {
     }
 
     @Override
-    public boolean onCommand(Message message, String[] args) {
-        if (message instanceof CommandMessage) {
-            CommandMessage commandMessage = (CommandMessage) message;
-
-            if (commandMessage.isMentionableCommand()) {
-                return sendErrorMessage(message, "This command can not be used via mentions!");
-            }
-        }
-
-        if (args.length == 0) {
-            return sendErrorMessage(message, "You must include the time you want the bot to shutdown.");
-        }
-
-        if (args[0].equalsIgnoreCase("now")) {
-            MessageFactory.makeInfo(message, "Shutting down processes... See you soon :wave:")
-                .queue(shutdownMessage -> avaire.shutdown(), throwable -> avaire.shutdown());
-
-            return true;
-        }
-
-        if (args[0].equalsIgnoreCase("cancel")) {
-            MessageFactory.makeInfo(message, "The shutdown process has been canceled.")
-                .queue(
-                    shutdownMessage -> avaire.scheduleShutdown(null, ExitCodes.EXIT_CODE_RESTART),
-                    throwable -> avaire.scheduleShutdown(null, ExitCodes.EXIT_CODE_RESTART)
-                );
-
-            return true;
-        }
-
-        Carbon time = formatInput(String.join(" ", args));
-        if (time == null) {
-            return sendErrorMessage(message, "Invalid time format given, `%s` is not a valid supported time format.",
-                String.join(" ", args)
-            );
-        }
-
-        if (time.isPast()) {
-            return sendErrorMessage(message, "The time given is in the past, that doesn't really work... Use a time set in the future, or use `now`.");
-        }
-
-        MessageFactory.makeSuccess(message, "The bot has been scheduled to restart in :fromNow.\n**Date:** :date")
-            .set("fromNow", time.diffForHumans(true))
-            .set("date", time.format("EEEEEEEE, dd MMMMMMM yyyy - HH:mm:ss z"))
-            .queue(
-                shutdownMessage -> avaire.scheduleShutdown(time, ExitCodes.EXIT_CODE_RESTART),
-                throwable -> avaire.scheduleShutdown(time, ExitCodes.EXIT_CODE_RESTART)
-            );
-
-        return true;
+    public int exitCode() {
+        return ExitCodes.EXIT_CODE_NORMAL;
     }
 
-    private Carbon formatInput(String time) {
-        for (Formats format : Formats.values()) {
-            try {
-                return Carbon.createFromFormat(format.getFormat(), time);
-            } catch (ParseException ignored) {
-            }
-        }
-        return null;
+    @Override
+    public String shutdownNow() {
+        return "Shutting down processes... See you soon :wave:";
+    }
+
+    @Override
+    public String scheduleShutdown() {
+        return "The bot has been scheduled to shutdown in :fromNow.\n**Date:** :date";
+    }
+
+    @Override
+    public String scheduleCancel() {
+        return "The shutdown process has been canceled.";
     }
 }
---
+++ src/main/java/com/avairebot/contracts/commands/ApplicationShutdownCommand.java
@@ -0,0 +1,144 @@
+package com.avairebot.contracts.commands;
+
+import com.avairebot.AvaIre;
+import com.avairebot.commands.CommandMessage;
+import com.avairebot.factories.MessageFactory;
+import com.avairebot.shared.ExitCodes;
+import com.avairebot.time.Carbon;
+import com.avairebot.time.Formats;
+import net.dv8tion.jda.core.entities.Message;
+
+import java.text.ParseException;
+
+public abstract class ApplicationShutdownCommand extends SystemCommand {
+
+    /**
+     * Creates the given command instance by calling {@link #ApplicationShutdownCommand(AvaIre, boolean)} with allowDM set to true.
+     *
+     * @param avaire The AvaIre class instance.
+     */
+    public ApplicationShutdownCommand(AvaIre avaire) {
+        this(avaire, true);
+    }
+
+    /**
+     * Creates the given command instance with the given
+     * AvaIre instance and the allowDM settings.
+     *
+     * @param avaire  The AvaIre class instance.
+     * @param allowDM Determines if the command can be used in DMs.
+     */
+    public ApplicationShutdownCommand(AvaIre avaire, boolean allowDM) {
+        super(avaire, allowDM);
+    }
+
+    /**
+     * The command executor, this method is invoked by the command handler
+     * and the middleware stack when a user sends a message matching the
+     * commands prefix and one of its command triggers.
+     *
+     * @param message The JDA message object from the message received event.
+     * @param args    The arguments given to the command, if no arguments was given the array will just be empty.
+     * @return true on success, false on failure.
+     */
+    @Override
+    public boolean onCommand(Message message, String[] args) {
+        if (message instanceof CommandMessage) {
+            CommandMessage commandMessage = (CommandMessage) message;
+
+            if (commandMessage.isMentionableCommand()) {
+                return sendErrorMessage(message, "This command can not be used via mentions!");
+            }
+        }
+
+        if (args.length == 0) {
+            return sendErrorMessage(message, "You must include the time you want the bot to shutdown.");
+        }
+
+        if (args[0].equalsIgnoreCase("now")) {
+            MessageFactory.makeInfo(message, shutdownNow())
+                .queue(
+                    shutdownMessage -> avaire.shutdown(exitCode()),
+                    throwable -> avaire.shutdown(exitCode())
+                );
+
+            return true;
+        }
+
+        if (args[0].equalsIgnoreCase("cancel")) {
+            MessageFactory.makeInfo(message, scheduleCancel())
+                .queue(
+                    shutdownMessage -> avaire.scheduleShutdown(null, exitCode()),
+                    throwable -> avaire.scheduleShutdown(null, exitCode())
+                );
+
+            return true;
+        }
+
+        Carbon time = formatInput(String.join(" ", args));
+        if (time == null) {
+            return sendErrorMessage(message, "Invalid time format given, `%s` is not a valid supported time format.",
+                String.join(" ", args)
+            );
+        }
+
+        if (time.isPast()) {
+            return sendErrorMessage(message, "The time given is in the past, that doesn't really work... Use a time set in the future, or use `now`.");
+        }
+
+        MessageFactory.makeSuccess(message, scheduleShutdown())
+            .set("fromNow", time.diffForHumans(true))
+            .set("date", time.format("EEEEEEEE, dd MMMMMMM yyyy - HH:mm:ss z"))
+            .queue(
+                shutdownMessage -> avaire.scheduleShutdown(time, exitCode()),
+                throwable -> avaire.scheduleShutdown(time, exitCode())
+            );
+
+        return true;
+    }
+
+    /**
+     * Formats the input given by the user, trying to parse the time
+     * to any of the valid formats supported by {@link Carbon}.
+     *
+     * @param time The string version of the time.
+     * @return Possibly-null, A Carbon instance matching the given time, or {@code null}.
+     */
+    private Carbon formatInput(String time) {
+        for (Formats format : Formats.values()) {
+            try {
+                return Carbon.createFromFormat(format.getFormat(), time);
+            } catch (ParseException ignored) {
+            }
+        }
+        return null;
+    }
+
+    /**
+     * The {@link ExitCodes} that should be used when the bot shuts down.
+     *
+     * @return The exit code that should be used when the bot shuts down.
+     */
+    public abstract int exitCode();
+
+    /**
+     * The message that should be sent when the bot shuts down right now.
+     *
+     * @return The message that should be sent when the bot shuts down right now.
+     */
+    public abstract String shutdownNow();
+
+    /**
+     * The message that should be sent when the bot is scheduled to shutdown.
+     *
+     * @return The message that should be sent when the bot is scheduled to shutdown.
+     */
+    public abstract String scheduleShutdown();
+
+    /**
+     * The message that should be sent when a scheduled shutdown process is canceled.
+     *
+     * @return The message that should be sent when a scheduled shutdown process is canceled.
+     */
+    public abstract String scheduleCancel();
+}
