99308a5c7f85e3a439cdebdc32b8696cddebd031
Alexis Tan <alexis@sen-dev.com>
2018-02-13 03:32:23 +0100

    Add message if the help command is used while help cateogry is disabled

--- src/main/java/com/avairebot/middleware/isCategoryEnabled.java
+++ src/main/java/com/avairebot/middleware/isCategoryEnabled.java
@@ -1,11 +1,16 @@
 package com.avairebot.middleware;
 
 import com.avairebot.AvaIre;
+import com.avairebot.commands.CommandHandler;
+import com.avairebot.commands.administration.ToggleCategoryCommand;
 import com.avairebot.contracts.middleware.Middleware;
 import com.avairebot.database.controllers.GuildController;
 import com.avairebot.database.transformers.ChannelTransformer;
 import com.avairebot.database.transformers.GuildTransformer;
+import com.avairebot.factories.MessageFactory;
 import net.dv8tion.jda.core.entities.Message;
+
+import java.util.concurrent.TimeUnit;
 
 public class isCategoryEnabled extends Middleware {
 
@@ -34,6 +39,12 @@ public class isCategoryEnabled extends Middleware {
         }
 
         if (!channel.isCategoryEnabled(stack.getCommandContainer().getCategory())) {
+            if (isHelpCommand(stack) && stack.isMentionableCommand()) {
+                MessageFactory.makeError(message, "The help command is disable in this channel, you can enable it by using the `:category` command.")
+                    .set("category", CommandHandler.getCommand(ToggleCategoryCommand.class).getCommand().generateCommandTrigger(message))
+                    .queue(success -> success.delete().queueAfter(15, TimeUnit.SECONDS));
+            }
+
             return false;
         }
 
@@ -43,5 +54,9 @@ public class isCategoryEnabled extends Middleware {
     private boolean isCategoryCommands(MiddlewareStack stack) {
         return stack.getCommand().getClass().getTypeName().equals("com.avairebot.commands.administration.ToggleCategoryCommand") ||
             stack.getCommand().getClass().getTypeName().equals("com.avairebot.commands.administration.CategoriesCommand");
+    }
+
+    private boolean isHelpCommand(MiddlewareStack stack) {
+        return stack.getCommand().getClass().getTypeName().equals("com.avairebot.commands.help.HelpCommand");
     }
 }
