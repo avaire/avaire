6042125c7686e520d6811b465517876258d6060a
Alexis Tan <alexis@sen-dev.com>
2018-02-26 16:39:52 +0100

    Add image and hint for how to resolve missing embed links permissions

--- src/main/java/com/avairebot/middleware/ProcessCommand.java
+++ src/main/java/com/avairebot/middleware/ProcessCommand.java
@@ -43,7 +43,7 @@ public class ProcessCommand extends Middleware {
                 return false;
             }
 
-            message.getTextChannel().sendMessage("I don't have the `Embed Links` permission, the permission is required for all of my commands to work.\nThis message will be automatically deleted in 30 seconds.")
+            message.getTextChannel().sendMessage("I don't have the `Embed Links` permission, the permission is required for all of my commands to work.\nhttps://avairebot.com/missing-embed-permissions.png\nThis error can sometimes occur when the `everyone` role has it disabled an no other roles enables it.\nThis message will be automatically deleted in 30 seconds.")
                 .queue(newMessage -> newMessage.delete().queueAfter(30, TimeUnit.SECONDS));
 
             return false;
