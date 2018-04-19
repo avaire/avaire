ebecbe93b3342d8686c9b6d02702b3779cdb9f92
Alexis Tan <alexis@sen-dev.com>
2018-04-19 13:44:27 +0200

    Cleanup and add documentation to the garbage collector job

--- src/main/java/com/avairebot/scheduler/GarbageCollectorJob.java
+++ src/main/java/com/avairebot/scheduler/GarbageCollectorJob.java
@@ -2,6 +2,7 @@ package com.avairebot.scheduler;
 
 import com.avairebot.AvaIre;
 import com.avairebot.audio.AudioHandler;
+import com.avairebot.audio.AudioSession;
 import com.avairebot.audio.GuildMusicManager;
 import com.avairebot.audio.LavalinkManager;
 import com.avairebot.cache.CacheType;
@@ -15,59 +16,88 @@ import java.util.Map;
 
 public class GarbageCollectorJob extends Job {
 
-    private static int counter = 1;
-
     public GarbageCollectorJob(AvaIre avaire) {
         super(avaire);
     }
 
     @Override
     public void run() {
+        // Clears the user cache set for DM info messages, this will reset
+        // the list, allowing users to get the DM info message again.
         MessageEventAdapter.hasReceivedInfoMessageInTheLastMinute.clear();
 
+        // Remove cache entries from the memory cache adapter
+        // if the keys are still stored by has expired.
         MemoryAdapter adapter = (MemoryAdapter) avaire.getCache().getAdapter(CacheType.MEMORY);
-
         adapter.getCacheKeys().removeIf(key -> !adapter.has(key));
 
-        for (Map.Entry<Long, GuildMusicManager> entry : AudioHandler.MUSIC_MANAGER.entrySet()) {
-            if (entry.getValue() == null) {
-                AudioHandler.MUSIC_MANAGER.remove(entry.getKey());
-                continue;
-            }
-
-            if (entry.getValue().getLastActiveMessage() == null) {
-                continue;
-            }
-
-            if (LavalinkManager.LavalinkManagerHolder.LAVALINK.isEnabled()) {
-                if (isConnected(LavalinkManager.LavalinkManagerHolder.LAVALINK.getLavalink().getLink(
-                    entry.getValue().getLastActiveMessage().getGuild()
-                ).getState())) {
-                    continue;
-                }
-            } else {
-                if (isConnected(entry.getValue().getLastActiveMessage().getGuild().getAudioManager())) {
-                    continue;
-                }
-            }
-
-            AudioHandler.MUSIC_MANAGER.remove(entry.getKey());
-        }
-
-        // 25 seconds
-        AudioHandler.AUDIO_SESSION.entrySet()
-            .removeIf(next -> (next.getValue().getCreatedAt() + 25000) < System.currentTimeMillis());
-
-        if (counter++ % 60 == 0) {
-            System.gc();
-        }
+        // Clean music managers and audio sessions by removing
+        // them if they have expired or are unused.
+        AudioHandler.MUSIC_MANAGER.entrySet().removeIf(this::musicManagerFilter);
+        AudioHandler.AUDIO_SESSION.entrySet().removeIf(this::audioSessionFilter);
     }
 
+    /**
+     * Checks if the {@link AudioManager Audio Manager} is connected to a voice channel.
+     *
+     * @param audioManager The audio manager that should be checked.
+     * @return True if the audio manager is connected, false otherwise.
+     */
     private boolean isConnected(AudioManager audioManager) {
         return audioManager.isConnected() || audioManager.isAttemptingToConnect();
     }
 
+    /**
+     * Checks the {@link Link.State Lavalink State} to see
+     * if the client is connected to a voice channel.
+     *
+     * @param state The Lavalink state that should be checked.
+     * @return True if the client is connected, false otherwise.
+     */
     private boolean isConnected(Link.State state) {
         return state.equals(Link.State.CONNECTED) || state.equals(Link.State.CONNECTING);
+    }
+
+    /**
+     * Checks music managers if they should be removed from the cache, music managers
+     * are removed if the manager is null, or if the guild has a music manager
+     * but is not connected to a voice channel.
+     *
+     * @param next The music manager entry.
+     * @return True the music manager should be removed, false otherwise.
+     */
+    private boolean musicManagerFilter(Map.Entry<Long, GuildMusicManager> next) {
+        if (next.getValue() == null) {
+            return true;
+        }
+
+        if (next.getValue().getLastActiveMessage() == null) {
+            return false;
+        }
+
+        // If Lavalink is enabled we'll use the Lavalink link state
+        // for the current guild instead of the audio manager.
+        if (LavalinkManager.LavalinkManagerHolder.LAVALINK.isEnabled()) {
+            if (isConnected(LavalinkManager.LavalinkManagerHolder.LAVALINK.getLavalink().getLink(
+                next.getValue().getLastActiveMessage().getGuild()
+            ).getState())) {
+                return false;
+            }
+        } else {
+            if (isConnected(next.getValue().getLastActiveMessage().getGuild().getAudioManager())) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Removes audio sessions that has existed for 25 seconds or longer.
+     *
+     * @param next The audio session entry.
+     * @return True if the audio session has existed for 25 seconds or longer, false otherwise.
+     */
+    private boolean audioSessionFilter(Map.Entry<String, AudioSession> next) {
+        return (next.getValue().getCreatedAt() + 25000) < System.currentTimeMillis();
     }
 }
