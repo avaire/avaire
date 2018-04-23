303b051778c0b73f1e07f98cff544873a65c25b2
Alex Taxiera <alex.taxiera@gmail.com>
2018-04-23 17:07:12 -0400

    add null annotations

--- src/main/java/com/avairebot/contracts/database/grammar/Grammarable.java
+++ src/main/java/com/avairebot/contracts/database/grammar/Grammarable.java
@@ -1,4 +1,6 @@
 package com.avairebot.contracts.database.grammar;
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 
 import java.sql.SQLException;
 import java.util.Map;
@@ -8,13 +10,13 @@ import com.avairebot.database.query.QueryBuilder;
 import com.avairebot.database.schema.Blueprint;
 
 public interface Grammarable {
-    public String create(DatabaseManager manager, Blueprint blueprint, Map<String, Boolean> options) throws SQLException;
+    public String create(DatabaseManager manager, Blueprint blueprint, @Nonnull Map<String, Boolean> options) throws SQLException;
     
-    public String delete(DatabaseManager manager, QueryBuilder query, Map<String, Boolean> options) throws SQLException;
+    public String delete(DatabaseManager manager, QueryBuilder query, @Nullable Map<String, Boolean> options) throws SQLException;
     
-    public String insert(DatabaseManager manager, QueryBuilder query, Map<String, Boolean> options) throws SQLException;
+    public String insert(DatabaseManager manager, QueryBuilder query, @Nullable Map<String, Boolean> options) throws SQLException;
     
-    public String select(DatabaseManager manager, QueryBuilder query, Map<String, Boolean> options) throws SQLException;
+    public String select(DatabaseManager manager, QueryBuilder query, @Nullable Map<String, Boolean> options) throws SQLException;
     
-    public String update(DatabaseManager manager, QueryBuilder query, Map<String, Boolean> options) throws SQLException;
+    public String update(DatabaseManager manager, QueryBuilder query, @Nullable Map<String, Boolean> options) throws SQLException;
 }
