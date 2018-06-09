a0bba793fe5efd991c9073b218b6dc5a2763ef82
Alexis Tan <alexis@sen-dev.com>
2018-06-09 23:25:24 +0200

    Add info logger statement on post vote route
    This should help us see when a vote request comes in, regardless of if it has the right token or not.

--- src/main/java/com/avairebot/metrics/routes/PostVote.java
+++ src/main/java/com/avairebot/metrics/routes/PostVote.java
@@ -9,6 +9,8 @@ import com.avairebot.metrics.Metrics;
 import com.avairebot.utilities.RestActionUtil;
 import net.dv8tion.jda.core.entities.User;
 import org.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import spark.Request;
 import spark.Response;
 
@@ -17,33 +19,42 @@ import java.sql.SQLException;
 
 public class PostVote extends SparkRoute {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(PostVote.class);
+
     public PostVote(Metrics metrics) {
         super(metrics);
     }
 
     @Override
     public Object handle(Request request, Response response) throws Exception {
+        LOGGER.info("Vote route has been hit by {} with the body: {}",
+            request.ip(), request.body()
+        );
+
         if (!hasValidAuthorizationHeader(request)) {
+            LOGGER.warn("Unauthorized request, missing or invalid \"Authorization\" header give.");
             return buildResponse(response, 401, "Unauthorized request, missing or invalid \"Authorization\" header give.");
         }
 
         VoteRequest voteRequest = AvaIre.GSON.fromJson(request.body(), VoteRequest.class);
 
         if (!isValidVoteRequest(voteRequest)) {
+            LOGGER.warn("Bad request, invalid JSON data given to justify a upvote request.");
             return buildResponse(response, 400, "Bad request, invalid JSON data given to justify a upvote request.");
         }
 
         User userById = metrics.getAvaire().getShardManager().getUserById(voteRequest.user);
         if (userById == null || userById.isBot()) {
+            LOGGER.warn("Invalid user ID given, the user is not on any server the bot is on.");
             return buildResponse(response, 404, "Invalid user ID given, the user is not on any server the bot is on.");
         }
 
         int userPoints = getVotePoints(metrics.getAvaire(), userById);
         metrics.getAvaire().getVoteManager().registerVoteFor(userById);
 
-        AvaIre.getLogger().info(String.format("Vote has been registered by %s (%s)",
+        LOGGER.info("Vote has been registered by {} ({})",
             userById.getName() + "#" + userById.getDiscriminator(), userById.getId()
-        ));
+        );
 
         if (userPoints == Integer.MIN_VALUE) {
             return buildResponse(response, 200, "Vote registered, thanks for voting!");
