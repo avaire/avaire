0de40646d74fe1afb10ec8710e0df11c78f6d6ed
Alexis Tan <alexis@sen-dev.com>
2018-08-02 02:23:04 +0200

    Change twitch source manager to use the helix API route

--- src/main/java/com/avairebot/audio/source/twitch/TwitchStreamAudioSourceManager.java
+++ src/main/java/com/avairebot/audio/source/twitch/TwitchStreamAudioSourceManager.java
@@ -147,7 +147,7 @@ public class TwitchStreamAudioSourceManager implements AudioSourceManager, HttpC
 
     private JsonBrowser fetchStreamChannelInfo(String name) {
         try (HttpInterface httpInterface = getHttpInterface()) {
-            HttpUriRequest request = createGetRequest("https://api.twitch.tv/kraken/channels/" + name);
+            HttpUriRequest request = createGetRequest("https://api.twitch.tv/helix/streams?user_login=" + name);
 
             return HttpClientTools.fetchResponseAsJson(httpInterface, request);
         } catch (IOException e) {
