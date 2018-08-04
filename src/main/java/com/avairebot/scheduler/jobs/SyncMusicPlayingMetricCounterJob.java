3ee1a42fdf7b3632b25bdccdf8b29073b820bdf0
Alexis Tan <alexis@sen-dev.com>
2018-08-04 19:29:57 +0200

    Raise music metrics counter update delay from 5 seconds to 15 seconds

--- src/main/java/com/avairebot/scheduler/jobs/SyncMusicPlayingMetricCounterJob.java
+++ src/main/java/com/avairebot/scheduler/jobs/SyncMusicPlayingMetricCounterJob.java
@@ -11,7 +11,7 @@ import java.util.concurrent.TimeUnit;
 public class SyncMusicPlayingMetricCounterJob extends Job {
 
     public SyncMusicPlayingMetricCounterJob(AvaIre avaire) {
-        super(avaire, 5, 5, TimeUnit.SECONDS);
+        super(avaire, 5, 15, TimeUnit.SECONDS);
     }
 
     @Override
