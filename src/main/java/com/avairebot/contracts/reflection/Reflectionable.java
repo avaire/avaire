4667261387e6e06a01c91ed6817bf7da6829ac3d
Alexis Tan <alexis@sen-dev.com>
2018-08-23 21:00:28 +0200

    Refactor and fix formatting of some code

--- src/main/java/com/avairebot/contracts/reflection/Reflectionable.java
+++ src/main/java/com/avairebot/contracts/reflection/Reflectionable.java
@@ -3,7 +3,7 @@ package com.avairebot.contracts.reflection;
 import com.avairebot.AvaIre;
 
 public abstract class Reflectionable implements Reflectional {
-    
+
     /**
      * The AvaIre class instance, this is used to access
      * and interact with the rest of the application.
--- src/main/java/com/avairebot/database/query/JoinClause.java
+++ src/main/java/com/avairebot/database/query/JoinClause.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class JoinClause implements QueryClause {
-    
+
     /**
      * The type of join being performed.
      */
