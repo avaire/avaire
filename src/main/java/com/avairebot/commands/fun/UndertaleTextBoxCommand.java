a03191ab77da6777391c8d55ed654facfde98cc9
Alexis Tan <alexis@sen-dev.com>
2018-10-20 16:26:08 +0200

    Fix file being generated and sent correctly + Error messages

--- src/main/java/com/avairebot/commands/fun/UndertaleTextBoxCommand.java
+++ src/main/java/com/avairebot/commands/fun/UndertaleTextBoxCommand.java
@@ -3,8 +3,14 @@ package com.avairebot.commands.fun;
 import com.avairebot.AvaIre;
 import com.avairebot.commands.CommandMessage;
 import com.avairebot.contracts.commands.Command;
+import com.avairebot.language.I18n;
+import net.dv8tion.jda.core.EmbedBuilder;
+import net.dv8tion.jda.core.MessageBuilder;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
+import java.net.URL;
 import java.net.URLEncoder;
 import java.util.Arrays;
 import java.util.Collections;
@@ -12,7 +18,7 @@ import java.util.List;
 
 public class UndertaleTextBoxCommand extends Command {
 
-    private String templateUrl = "www.demirramon.com/utgen.png?message=%s&character=%s";
+    private final String templateUrl = "https://www.demirramon.com/gen/undertale_box.png?character={0}&message={1}&ext=.png";
 
     public UndertaleTextBoxCommand(AvaIre avaire) {
         super(avaire);
@@ -31,13 +37,13 @@ public class UndertaleTextBoxCommand extends Command {
     @Override
     public List<String> getUsageInstructions() {
         return Collections.singletonList(
-            "`:command [message] [character]` - Returns an undertale textbox image containing the specified text."
+            "`:command <character> <message>` - Returns an undertale textbox image containing the specified text."
         );
     }
 
     @Override
     public List<String> getExampleUsage() {
-        return Collections.singletonList("`:command toriel Greetings,my child`");
+        return Collections.singletonList("`:command toriel Greetings, my child`");
     }
 
     @Override
@@ -53,21 +59,41 @@ public class UndertaleTextBoxCommand extends Command {
     @Override
     public boolean onCommand(CommandMessage context, String[] args) {
         if (args.length == 0) {
-            return sendErrorMessage(context, context.i18n("missingArgument"));
+            return sendErrorMessage(context, "errors.missingArgument", "character");
+        }
+
+        if (args.length == 1) {
+            return sendErrorMessage(context, "errors.missingArgument", "message");
         }
 
         try {
-            context.makeEmbeddedMessage()
-                .setImage(String.format(
-                    templateUrl,
-                    URLEncoder.encode(args[0], "UTF-8").replace("+", "%20"),
-                    args[1].toLowerCase()
-                )).queue();
+            MessageBuilder messageBuilder = new MessageBuilder();
+            EmbedBuilder embedBuilder = context.makeEmbeddedMessage()
+                .setImage("attachment://" + getClass().getSimpleName() + "-" + args[0] + ".png")
+                .requestedBy(context)
+                .build();
+
+            messageBuilder.setEmbed(embedBuilder.build());
+
+            InputStream stream = new URL(I18n.format(
+                templateUrl,
+                encode(args[0]),
+                encode(String.join(" ", Arrays.copyOfRange(args, 1, args.length)))
+            )).openStream();
+
+            context.getChannel().sendFile(stream, getClass().getSimpleName() + "-" + args[0] + ".png", messageBuilder.build()).queue();
+
+            return true;
         } catch (UnsupportedEncodingException e) {
             context.makeError(e.getMessage());
-            return false;
+        } catch (IOException e) {
+            context.makeError(e.getMessage());
         }
 
-        return true;
+        return false;
+    }
+
+    private String encode(String string) throws UnsupportedEncodingException {
+        return URLEncoder.encode(string, "UTF-8").replace("+", "%20");
     }
 }
