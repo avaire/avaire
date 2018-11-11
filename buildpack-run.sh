ec34859a8987f69cce4d62026fc289e863fc9cd6
Macley <26381427+Macley-Kun@users.noreply.github.com>
2018-11-11 16:11:34 +0100

    Perhapes
    Yes.

--- buildpack-run.sh
+++ buildpack-run.sh
@@ -1,5 +1,5 @@
 # This is buildpack-run.sh
-git clone https://github.com/Macley-Kun/avaire.git
+git clone https://github.com/avaire/avaire.git
 cp -r avaire/.git .
 cp src/main/resources/config.yml .
 rm -rf avaire/
