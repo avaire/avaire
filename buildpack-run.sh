395383970964d6317ae7b4c26c85d3ebe66885e7
Macley <26381427+Macley-Kun@users.noreply.github.com>
2018-11-11 19:20:39 +0100

    Update buildpack-run.sh
    I've updated the file b/c i had some strange issues.
    As of now, i don't have any issues anymore. But this form should not only verify that the script always works. But also shows it in the log.

--- buildpack-run.sh
+++ buildpack-run.sh
@@ -1,5 +1,2 @@
 # This is buildpack-run.sh
-git clone https://github.com/avaire/avaire.git
-cp -r avaire/.git .
-cp src/main/resources/config.yml .
-rm -rf avaire/
+rm -rf .git && rm -rf build/ && git clone https://github.com/avaire/avaire.git && cp -r avaire/.git . && cp src/main/resources/config.yml . && rm -rf avaire/ && echo script worked.
