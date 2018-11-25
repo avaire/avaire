a3c48b664f6b0171092cf31d440c3ad25efce641
Alexis Tan <alexis@sen-dev.com>
2018-11-25 17:40:45 +0100

    Create reaction controller and transformers

---
+++ src/main/java/com/avairebot/database/controllers/ReactionController.java
@@ -0,0 +1,94 @@
+/*
+ * Copyright (c) 2018.
+ *
+ * This file is part of AvaIre.
+ *
+ * AvaIre is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * AvaIre is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with AvaIre.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ *
+ */
+
+package com.avairebot.database.controllers;
+
+import com.avairebot.AvaIre;
+import com.avairebot.Constants;
+import com.avairebot.database.collection.Collection;
+import com.avairebot.database.collection.DataRow;
+import com.avairebot.database.transformers.ReactionTransformer;
+import com.avairebot.utilities.CacheUtil;
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
+import net.dv8tion.jda.core.entities.Guild;
+import net.dv8tion.jda.core.entities.Message;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.annotation.CheckReturnValue;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+public class ReactionController {
+
+    public static final Cache<Long, Collection> cache = CacheBuilder.newBuilder()
+        .recordStats()
+        .expireAfterAccess(5, TimeUnit.MINUTES)
+        .build();
+
+    private static final Logger log = LoggerFactory.getLogger(ReactionController.class);
+
+    @CheckReturnValue
+    public static Collection fetchReactions(AvaIre avaire, Message message) {
+        if (!message.getChannelType().isGuild()) {
+            return null;
+        }
+        return fetchReactions(avaire, message.getGuild());
+    }
+
+    @CheckReturnValue
+    public static Collection fetchReactions(AvaIre avaire, Guild guild) {
+        return (Collection) CacheUtil.getUncheckedUnwrapped(cache, guild.getIdLong(), () -> {
+            log.debug("Guild Reaction cache for " + guild.getId() + " was refreshed");
+
+            try {
+                return avaire.getDatabase()
+                    .newQueryBuilder(Constants.REACTION_ROLES_TABLE_NAME)
+                    .selectAll()
+                    .where("guild_id", guild.getId())
+                    .get();
+            } catch (Exception ex) {
+                AvaIre.getLogger().error(ex.getMessage(), ex);
+                return null;
+            }
+        });
+    }
+
+    @CheckReturnValue
+    public static ReactionTransformer fetchReactionFromMessage(AvaIre avaire, Message message) {
+        Collection reactions = fetchReactions(avaire, message);
+        if (reactions == null) {
+            return null;
+        }
+
+        List<DataRow> reaction = reactions.where("message_id", message.getIdLong());
+        if (reaction.isEmpty()) {
+            return null;
+        }
+
+        return new ReactionTransformer(reaction.get(0));
+    }
+
+    public static void forgetCache(long guildId) {
+        cache.invalidate(guildId);
+    }
+}
---
+++ src/main/java/com/avairebot/database/transformers/ReactionTransformer.java
@@ -0,0 +1,92 @@
+/*
+ * Copyright (c) 2018.
+ *
+ * This file is part of AvaIre.
+ *
+ * AvaIre is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * AvaIre is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with AvaIre.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ *
+ */
+
+package com.avairebot.database.transformers;
+
+import com.avairebot.AvaIre;
+import com.avairebot.contracts.database.transformers.Transformer;
+import com.avairebot.database.collection.DataRow;
+import com.google.gson.reflect.TypeToken;
+import net.dv8tion.jda.core.entities.Emote;
+import net.dv8tion.jda.core.entities.Role;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ReactionTransformer extends Transformer {
+
+    private final Map<Long, Long> roles = new HashMap<>();
+
+    private long guildId = -1;
+    private long channelId = -1;
+    private long messageId = -1;
+
+    public ReactionTransformer(DataRow row) {
+        super(row);
+
+        if (row != null) {
+            guildId = row.getLong("guild_id");
+            channelId = row.getLong("channel_id");
+            messageId = row.getLong("message_id");
+
+            if (data.getString("roles", null) != null) {
+                HashMap<Long, Long> dbRoles = AvaIre.gson.fromJson(
+                    data.getString("roles"),
+                    new TypeToken<HashMap<Long, Long>>() {
+                    }.getType());
+
+                for (Map.Entry<Long, Long> item : dbRoles.entrySet()) {
+                    roles.put(item.getKey(), item.getValue());
+                }
+            }
+        }
+    }
+
+    @Nullable
+    public Long getRoleIdFromEmote(@Nonnull Emote emote) {
+        if (!roles.containsKey(emote.getIdLong())) {
+            return null;
+        }
+        return roles.get(emote.getIdLong());
+    }
+
+    public void addReaction(@Nonnull Emote emote, @Nonnull Role role) {
+        roles.put(emote.getIdLong(), role.getIdLong());
+    }
+
+    public Map<Long, Long> getRoles() {
+        return roles;
+    }
+
+    public long getGuildId() {
+        return guildId;
+    }
+
+    public long getChannelId() {
+        return channelId;
+    }
+
+    public long getMessageId() {
+        return messageId;
+    }
+}
