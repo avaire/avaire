eab5cdb7808a4c0843b927e7945cd534486225ff
chaNcharge <chancharge@gmail.com>
2018-12-20 14:11:30 -0800

    Add branch variables

--- update.sh
+++ update.sh
@@ -9,8 +9,8 @@ git config --global user.email "updater@updater" && git config --global user.nam
 git clone "https://github.com/$GITHUB_REPO" "$repo_temp"
 cd "$repo_temp"
 
-git checkout -b upstream-master master
-git pull --no-edit $UPSTREAM_REPO master
+git checkout -b upstream $GITHUB_REPO_BRANCH
+git pull --no-edit $UPSTREAM_REPO $UPSTREAM_BRANCH
 
 CONFLICTS=$(git ls-files -u | wc -l)
 if [ "$CONFLICTS" -gt 0 ] ; then
@@ -20,8 +20,8 @@ if [ "$CONFLICTS" -gt 0 ] ; then
     exit 1
 fi
 
-git checkout master
-git merge --no-edit --no-ff upstream-master
+git checkout $GITHUB_REPO_BRANCH
+git merge --no-edit --no-ff upstream
 
 # Redirect to /dev/null to avoid secret leakage
-git push "$push_uri" master >/dev/null 2>&1
+git push "$push_uri" $GITHUB_REPO_BRANCH >/dev/null 2>&1
