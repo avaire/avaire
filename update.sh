bc2aaca997205fddd1dc240cfce6fddf03a66fab
chaNcharge <chancharge@gmail.com>
2019-01-17 14:37:14 -0800

    Fix duplicate issue creation for real this time

--- update.sh
+++ update.sh
@@ -16,8 +16,8 @@ CONFLICTS=$(git ls-files -u | wc -l)
 if [ "$CONFLICTS" -gt 0 ] ; then
     echo "There is a merge conflict. Aborting"
     git merge --abort
-    conflicts=$(curl https://api.github.com/search/issues?q=is:issue+is:open+repo:$GITHUB_REPO+author:$GITHUB_USER+label:merge-conflict | grep "url") # I would use jq, but for simple heroku purposes, I'll use a dirty way
-    if [ -n "$var" ] ; then
+    conflictcheck=$(curl https://api.github.com/search/issues\?q\=is:issue+is:open+repo:$GITHUB_REPO+author:$GITHUB_USER+label:merge-conflict | python -c 'import json,sys;obj=json.load(sys.stdin);print obj["total_count"]') # I would use jq, but to minimize deps, I'll use a dirty way
+    if [ $conflictcheck -eq 0 ] ; then
         curl -u $GITHUB_USER:$GITHUB_SECRET_TOKEN -H "Content-Type: application/json" -X POST -d '{"title": "Merge conflict detected", "body": "ForkUpdater could not update your repo. Please check for merge conflicts and update manually!","labels": ["merge-conflict"]}' https://api.github.com/repos/$GITHUB_REPO/issues
     fi
     exit 1
@@ -26,5 +26,5 @@ fi
 git checkout $GITHUB_REPO_BRANCH
 git merge --no-edit --no-ff upstream
 
-# Redirect to /dev/null to avoid secret leakage
+Redirect to /dev/null to avoid secret leakage
 git push "$push_uri" $GITHUB_REPO_BRANCH >/dev/null 2>&1
