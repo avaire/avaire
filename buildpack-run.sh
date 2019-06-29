875dfcefcff7f306a9ccf5308deaed976fb2441d
Macley <26381427+Macley-Kun@users.noreply.github.com>
2019-06-29 23:13:55 +0200

    Update buildpack-run.sh
    Change needed as this file is now marked missing. Request pulling this ASAP.

--- buildpack-run.sh
+++ buildpack-run.sh
@@ -1,5 +1,5 @@
 # This is buildpack-run.sh
 # Trick to clone AvaIre's git into a seperate folder, copying .git to the top directory. Followed by putting the config.yml on the right place.
-rm -rf .git && git clone https://github.com/avaire/avaire.git && cp -r avaire/.git . && cp src/main/resources/config.yml . && rm -rf avaire/ && echo script worked.
+rm -rf .git && git clone https://github.com/avaire/avaire.git && cp -r avaire/.git . && cp src/main/resources/config.yml . && cp src/main/resources/constants.yml . && rm -rf avaire/ && echo script worked.
 # Cleaning any build artifacts, done before and after the gradle proces.
 rm -rf build/ && echo cleaned out build artifacts.
