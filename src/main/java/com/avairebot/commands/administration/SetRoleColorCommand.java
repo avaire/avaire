05b63c72913a38e1ddea76af816438bb12719c8f
Aaron <a.rcottrill521@gmail.com>
2019-07-10 21:02:48 -0400

    Update SetRoleColorCommand.java
    Fix the color and colour situation as well as add the AvaIre header.

--- src/main/java/com/avairebot/commands/administration/SetRoleColorCommand.java
+++ src/main/java/com/avairebot/commands/administration/SetRoleColorCommand.java
@@ -1,3 +1,24 @@
+/*
+ * Copyright (c) 2019.
+ *
+ * This file is part of AvaIre.
+ *
+ * AvaIre is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * AvaIre is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with AvaIre.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ *
+ */
+
 package com.avairebot.commands.administration;
 
 import com.avairebot.AvaIre;
@@ -26,7 +47,7 @@ public class SetRoleColorCommand extends Command {
 
     @Override
     public String getDescription() {
-        return "Takes in a role name, followed by a HEX value to set a role to a specific colour by simply knowing the HEX value. Additionally users can specify a random color.";
+        return "Takes in a role name, followed by a HEX value to set a role to a specific colour by simply knowing the HEX value. Additionally users can specify a random colour.";
     }
 
     @Override
