e678a532f98b92e7bb2222d84a19dbdcfb1daf8c
Piecord <51022454+Piecord@users.noreply.github.com>
2019-08-12 00:12:22 +0100

    Update HelpCommand.java

--- src/main/java/com/avairebot/commands/help/HelpCommand.java
+++ src/main/java/com/avairebot/commands/help/HelpCommand.java
@@ -55,7 +55,7 @@ public class HelpCommand extends Command {
 
     @Override
     public String getDescription() {
-        return "Tells you about what commands AvaIre has, what they do, and how you can use them.";
+        return "Tells you about what commands AbBot has, what they do, and how you can use them.";
     }
 
     @Override
