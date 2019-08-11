5e7778a8ae42fcfd25467ce73496d51ebfb09210
Piecord <51022454+Piecord@users.noreply.github.com>
2019-08-11 23:56:41 +0100

    Update AliasCommand.java

--- src/main/java/com/avairebot/commands/administration/AliasCommand.java
+++ src/main/java/com/avairebot/commands/administration/AliasCommand.java
@@ -63,7 +63,7 @@ public class AliasCommand extends Command {
 
     @Override
     public List<String> getExampleUsage() {
-        return Collections.singletonList("`:command !ava !repeat **Website:** https://avairebot.com/`");
+        return Collections.singletonList("`:command !ava !repeat **Website:** https://abbot.ml/`");
     }
 
     @Override
