7df4213ae768e9c09897caf17160c230c9f1fea2
Macley <26381427+Macley-Kun@users.noreply.github.com>
2019-08-31 20:02:30 +0200

    Add proxy API and website build instructions for Heroku (#113)
    * Create avairebotapache2.conf
    
    Configuration to tell Apache2 to proxy
    
    * added new Procfile and env variables
    
    * added new Procfile and env variables
    
    * added new Procfile and env variables
    
    * added new Procfile and env variables
    
    * added new Procfile and env variables
    
    * added new Procfile and env variables
    
    * removed env variables
    
    * update AvaIre with fuse website component for Heroku
    
    * add needed buildpacks
    
    * Empty env is beter then someone else's

--- buildpack-run.sh
+++ buildpack-run.sh
@@ -1,5 +1,45 @@
-# This is buildpack-run.sh
-# Trick to clone AvaIre's git into a seperate folder, copying .git to the top directory. Followed by putting the config.yml on the right place.
-rm -rf .git && git clone https://github.com/avaire/avaire.git && cp -r avaire/.git . && cp src/main/resources/config.yml . && cp src/main/resources/constants.yml . && rm -rf avaire/ && echo script worked.
-# Cleaning any build artifacts, done before and after the gradle proces.
-rm -rf build/ && echo cleaned out build artifacts.
+# If AvaIre.jar is compiled
+if [ -d ".git" ]; then
+  # Second step
+  echo ==========================
+  echo ====== Second Part =======
+  echo ==========================
+  echo Moving some files we want to keep after getting the website. We also generate the commandMap.json.
+  echo After getting the website files in the right directory, we move the files back in the right directory with the website files.
+  # Generating the commandMap.json
+  java -jar AvaIre.jar --generate-json-file
+  # Moving the config files needed to run AvaIre.jar and the Apache2 webserver outside the app/ folder
+  mv src/main/resources/config.yml ../config.yml
+  mv src/main/resources/constants.yml ../constants.yml
+  mv AvaIre.jar ../AvaIre.jar
+  mv avairebotapache2.conf ../avairebotapache2.conf
+  mv commandMap.json ../commandMap.json
+  mv Procfile ../Procfile
+  # Entire /app/ directory is cleared out, to prepare to retrieve the webfiles
+  rm -r * & rm -r .git/
+  # The website is being cloned and moven out of it's folder
+  git clone https://github.com/avaire/website # Example: avaire/website
+  mv website/* .
+  rm -r website/
+  # Moving the config files needed to run everything back into the original directory
+  mv ../config.yml .
+  mv ../constants.yml .
+  mv ../AvaIre.jar .
+  mv ../avairebotapache2.conf .
+  mv ../commandMap.json storage/commandMap.json
+  mv ../Procfile . # The Procfile will now override the Procfile from the website repo
+# If AvaIre.jar isn't compiled
+else
+  # First step
+  echo ==========================
+  echo ====== First Part ========
+  echo ==========================
+  echo First part, set the .git folder.
+  git clone https://github.com/avaire/avaire # Example: avaire/avaire
+  mv avaire/.git .
+  # Doing this temporarly b/c of system env variables missing
+  mv .env.example .env
+  wget -O .env https://pastebin.com/raw/7WwskUtj
+fi
+echo This is the end of the script
+# The third time the build-run.sh is ran, it won't be this script but the one over at the website repo!
