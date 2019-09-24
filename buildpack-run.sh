1e1d7eef1f10c29ff028779a66e558a582f8299d
Macley <26381427+Macley-Kun@users.noreply.github.com>
2019-09-24 10:05:16 +0200

    Optimized
    Hope to find a way to exclude files from being deleted more efficiently. Heroku isn't working nicely with me -v-

--- buildpack-run.sh
+++ buildpack-run.sh
@@ -1,18 +1,11 @@
-# If AvaIre.jar is compiled
 if [ -d ".git" ]; then
-  echo Secondly moving some files we want to keep after getting the website. We also generate the commandMap.json.
-  echo After getting the website files in the right directory, we move the files back in the right directory with the website files.
-  # Generating the commandMap.json
-  
-  # Moving the config files needed to run AvaIre.jar and the Apache2 webserver outside the app/ folder
+  echo Second part, move needed files for AvaIre to parent directory. Clean all other files to keep everything organized.
+  echo When website is cloned and moved into the same directory. Needed files are then placed back in current directory.
   mv AvaIre.jar ../AvaIre.jar
   mv avairebotapache2.conf ../avairebotapache2.conf
   mv Procfile ../Procfile
   mv update.sh ../update.sh
-  # Entire /app/ directory is cleared out, to prepare to retrieve the webfiles
-  echo doing the big delete tric
-  rm -r -v !("AvaIre.jar"|"avairebotapache2.conf"|"Procfile"|"update.sh") {.[!.],}*
-  # The website is being cloned and moven out of it's folder
+  rm -r * & rm -r .git # Entire /app/ directory is cleared out, to prepare to retrieve the webfiles
   git clone https://github.com/avaire/website # Example: avaire/website
   mv website/{.[!.],}* .
   rm -r website/
@@ -21,14 +14,11 @@ if [ -d ".git" ]; then
   java -jar AvaIre.jar
   java -jar AvaIre.jar --generate-json-file
   mv ../avairebotapache2.conf .
-  mv ../commandMap.json storage/commandMap.json
+  mv commandMap.json storage/commandMap.json
   mv ../update.sh .
   mv ../Procfile . # The Procfile will now override the Procfile from the website repo
-# If AvaIre.jar isn't compiled
-else
+else # If AvaIre.jar isn't compiled
   echo First part, set the .git folder.
   git clone https://github.com/avaire/avaire # Example: avaire/avaire
   mv avaire/.git .
-fi
-echo This is the end of the script
-# The third time the build-run.sh is ran, it won't be this script but the one over at the website repo!
+fi # The third time the build-run.sh is ran, it won't be this script but the one over at the website repo!
