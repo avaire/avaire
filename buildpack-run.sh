b8fb6e2320176062227bc64c35ebabb491bbdc8d
Macley <26381427+Macley-Kun@users.noreply.github.com>
2019-09-24 17:51:27 +0200

    Update buildpack-run.sh

--- buildpack-run.sh
+++ buildpack-run.sh
@@ -1,7 +1,7 @@
 if [ -d ".git" ]; then # After compile remove .git (as it will be replaced) and delete everything else with exception after $. Get website except wrong Procfile for this build
   rm -r .git/ & rm -rf $(find * -name "*" ! -name "AvaIre.jar" ! -name "avairebotapache2.conf" ! -name "Procfile" ! -name "update.sh")
   git clone https://github.com/avaire/website && rm website/Procfile && mv website/{.[!.],}* . && rm -r website/ # Example: avaire/website
-  java -jar AvaIre.jar & java -jar AvaIre.jar --generate-json-file && mv commandMap.json storage/commandMap.json # Get needed configs and move command file to right directory
+  java -jar AvaIre.jar --generate-json-file && mv commandMap.json storage/commandMap.json # Get needed configs and move command file to right directory
 else # If AvaIre.jar isn't compiled
   git clone https://github.com/avaire/avaire && mv avaire/.git . # Example: avaire/avaire
 fi # The third time the build-run.sh is ran, it won't be this script but the one over at the website repo!
