df358ef1d586130ce74c89251be22c99cc7ee6d7
Macley <26381427+Macley-Kun@users.noreply.github.com>
2019-09-24 09:03:32 +0200

    Error less moving files!

--- buildpack-run.sh
+++ buildpack-run.sh
@@ -20,8 +20,7 @@ if [ -d ".git" ]; then
   rm -r * & rm -r .git/
   # The website is being cloned and moven out of it's folder
   git clone https://github.com/avaire/website # Example: avaire/website
-  mv website/* .
-  mv website/.* .
+  mv website/{.[!.],}* .
   rm -r website/
   # Moving the config files needed to run everything back into the original directory
   mv ../config.yml .
