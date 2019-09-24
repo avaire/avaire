998f7540a38dd031c9f824402379031ccaa06203
Macley <26381427+Macley-Kun@users.noreply.github.com>
2019-09-24 21:55:47 +0200

    Move hidden file for Heroku buildpack (#118)
    * Also move hidden files
    
    This is the last pull to fix the small issues that have come with moving to the new deploy strategie.
    
    I also found some improvements out, i list them down here, if you confirm it's true that avaire makes the missing .yml files herself.
    If your ok to have the update.sh also on the website repo i can remove that command here aswell.
    
    Proposed improvements:
    i could run avaire a second time after the commandmap.json, so it makes the following automatically and more scableable:
      
     mv src/main/resources/config.yml ../config.yml
     mv src/main/resources/constants.yml ../constants.yml
    
      mv ../config.yml .
      mv ../constants.yml .
    
    I just need to know for sure, avaire generates the missing yml files if it's ran withoud them?
    
    mv update.sh ../update.sh
    
    can maybe be removed if your ok to have this file in the website repo aswell!
    
    * Error less moving files!
    
    * Generate files with avaire
    
    Instead of moving it ourselfs, we let avaire do it :D
    
    * Optimized
    
    Hope to find a way to exclude files from being deleted more efficiently. Heroku isn't working nicely with me -v-
    
    * Improved script to the max
    
    So the past hours i made the following happen:
    we clone avaire to get the .git folder.
    After compiling we delete this folder, as i rather delete and create a new one for the website, then merge and hope it's all clean.
    We also delete everything that isn't hidden except for the file names after each ! (see line 2).
    Then we clone the website repo, remove the procfile b/c it's wrong for this build. We move everything out of this folder and delete it.
    We run both avaire to get the configs and to get the commandmap.json, we move that to the right place and it's all done.
    
    What does this bring?
    Shorter steps, flexible if another exception needs to be made. Just add after a ", this: ` ! -name "filenameorwhatever"` and it's done.
    Config files are done automatically, so no maintance on that anymore!
    
    Hope it's to your liking, it's been tested througly.
    
    * Update buildpack-run.sh

--- buildpack-run.sh
+++ buildpack-run.sh
@@ -1,44 +1,7 @@
-# If AvaIre.jar is compiled
-if [ -d ".git" ]; then
-  # Second step
-  echo ==========================
-  echo ====== Second Part =======
-  echo ==========================
-  echo Moving some files we want to keep after getting the website. We also generate the commandMap.json.
-  echo After getting the website files in the right directory, we move the files back in the right directory with the website files.
-  # Generating the commandMap.json
-  java -jar AvaIre.jar --generate-json-file
-  # Moving the config files needed to run AvaIre.jar and the Apache2 webserver outside the app/ folder
-  mv src/main/resources/config.yml ../config.yml
-  mv src/main/resources/constants.yml ../constants.yml
-  mv AvaIre.jar ../AvaIre.jar
-  mv avairebotapache2.conf ../avairebotapache2.conf
-  mv commandMap.json ../commandMap.json
-  mv Procfile ../Procfile
-  mv update.sh ../update.sh
-  # Entire /app/ directory is cleared out, to prepare to retrieve the webfiles
-  rm -r * & rm -r .git/
-  # The website is being cloned and moven out of it's folder
-  git clone https://github.com/avaire/website # Example: avaire/website
-  mv website/* .
-  rm -r website/
-  # Moving the config files needed to run everything back into the original directory
-  mv ../config.yml .
-  mv ../constants.yml .
-  mv ../AvaIre.jar .
-  mv ../avairebotapache2.conf .
-  mv ../commandMap.json storage/commandMap.json
-  mv ../update.sh .
-  mv ../Procfile . # The Procfile will now override the Procfile from the website repo
-# If AvaIre.jar isn't compiled
-else
-  # First step
-  echo ==========================
-  echo ====== First Part ========
-  echo ==========================
-  echo First part, set the .git folder.
-  git clone https://github.com/avaire/avaire # Example: avaire/avaire
-  mv avaire/.git .
-fi
-echo This is the end of the script
-# The third time the build-run.sh is ran, it won't be this script but the one over at the website repo!
+if [ -d ".git" ]; then # After compile remove .git (as it will be replaced) and delete everything else with exception after $. Get website except wrong Procfile for this build
+  rm -r .git/ & rm -rf $(find * -name "*" ! -name "AvaIre.jar" ! -name "avairebotapache2.conf" ! -name "Procfile" ! -name "update.sh")
+  git clone https://github.com/avaire/website && rm website/Procfile && mv website/{.[!.],}* . && rm -r website/ # Example: avaire/website
+  java -jar AvaIre.jar --generate-json-file && mv commandMap.json storage/commandMap.json # Get needed configs and move command file to right directory
+else # If AvaIre.jar isn't compiled
+  git clone https://github.com/avaire/avaire && mv avaire/.git . # Example: avaire/avaire
+fi # The third time the build-run.sh is ran, it won't be this script but the one over at the website repo!
