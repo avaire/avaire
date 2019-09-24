cd30fa3a88e45e2053297e3d3a57d593097fdab7
Macley <26381427+Macley-Kun@users.noreply.github.com>
2019-09-24 10:47:20 +0200

    Improved script to the max
    So the past hours i made the following happen:
    we clone avaire to get the .git folder.
    After compiling we delete this folder, as i rather delete and create a new one for the website, then merge and hope it's all clean.
    We also delete everything that isn't hidden except for the file names after each ! (see line 2).
    Then we clone the website repo, remove the procfile b/c it's wrong for this build. We move everything out of this folder and delete it.
    We run both avaire to get the configs and to get the commandmap.json, we move that to the right place and it's all done.
    
    What does this bring?
    Shorter steps, flexible if another exception needs to be made. Just add after a ", this: ` ! -name "filenameorwhatever"` and it's done.
    Config files are done automatically, so no maintance on that anymore!
    
    Hope it's to your liking, it's been tested througly.

--- buildpack-run.sh
+++ buildpack-run.sh
@@ -1,24 +1,7 @@
-if [ -d ".git" ]; then
-  echo Second part, move needed files for AvaIre to parent directory. Clean all other files to keep everything organized.
-  echo When website is cloned and moved into the same directory. Needed files are then placed back in current directory.
-  mv AvaIre.jar ../AvaIre.jar
-  mv avairebotapache2.conf ../avairebotapache2.conf
-  mv Procfile ../Procfile
-  mv update.sh ../update.sh
-  rm -r * & rm -r .git # Entire /app/ directory is cleared out, to prepare to retrieve the webfiles
-  git clone https://github.com/avaire/website # Example: avaire/website
-  mv website/{.[!.],}* .
-  rm -r website/
-  # Moving the config files needed to run everything back into the original directory
-  mv ../AvaIre.jar .
-  java -jar AvaIre.jar
-  java -jar AvaIre.jar --generate-json-file
-  mv ../avairebotapache2.conf .
-  mv commandMap.json storage/commandMap.json
-  mv ../update.sh .
-  mv ../Procfile . # The Procfile will now override the Procfile from the website repo
+if [ -d ".git" ]; then # After compile remove .git (as it will be replaced) and delete everything else with exception after $. Get website except wrong Procfile for this build
+  rm -r .git/ & rm -rf $(find * -name "*" ! -name "AvaIre.jar" ! -name "avairebotapache2.conf" ! -name "Procfile" ! -name "update.sh")
+  git clone https://github.com/avaire/website && rm website/Procfile && mv website/{.[!.],}* . && rm -r website/ # Example: avaire/website
+  java -jar AvaIre.jar & java -jar AvaIre.jar --generate-json-file && mv commandMap.json storage/commandMap.json # Get needed configs and move command file to right directory
 else # If AvaIre.jar isn't compiled
-  echo First part, set the .git folder.
-  git clone https://github.com/avaire/avaire # Example: avaire/avaire
-  mv avaire/.git .
+  git clone https://github.com/avaire/avaire && mv avaire/.git . # Example: avaire/avaire
 fi # The third time the build-run.sh is ran, it won't be this script but the one over at the website repo!
