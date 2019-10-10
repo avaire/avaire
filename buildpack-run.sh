7fdeeda7c4b6f8758d759c9428bf743b2018866e
Macley <26381427+Macley-Kun@users.noreply.github.com>
2019-10-10 23:32:18 +0200

    Fix buildpack permission issue for Heroku (#120)
    * Actually specifiy how to run this
    
    Partly fixes the run builpack issue.
    
    * chmod +x buildpack-run.sh
    
    * Update buildpack-run.sh

--- buildpack-run.sh
+++ buildpack-run.sh
@@ -1,5 +1,6 @@
+#!/bin/bash
 if [ -d ".git" ]; then # After compile remove .git (as it will be replaced) and delete everything else with exception after $. Get website except wrong Procfile for this build
-  rm -r .git/ & rm -rf $(find * -name "*" ! -name "AvaIre.jar" ! -name "avairebotapache2.conf" ! -name "Procfile" ! -name "update.sh")
+  rm -r .git/ & rm -rf $(find * -name "*" ! -name "AvaIre.jar" ! -name "avairebotapache2.conf" ! -name "Procfile" ! -name "update.sh" ! -name "plugins/*")
   git clone https://github.com/avaire/website && rm website/Procfile && mv website/{.[!.],}* . && rm -r website/ # Example: avaire/website
   java -jar AvaIre.jar --generate-json-file && mv commandMap.json storage/commandMap.json # Get needed configs and move command file to right directory
 else # If AvaIre.jar isn't compiled
