d34841fae79b34de3f6cb8075b5c4c9f74fbc2a8
Alexis Tan <alexis@sen-dev.com>
2019-10-27 14:29:05 +0100

    Add documentation to the scheduler handler

--- src/main/java/com/avairebot/scheduler/ScheduleHandler.java
+++ src/main/java/com/avairebot/scheduler/ScheduleHandler.java
@@ -40,14 +40,33 @@ public class ScheduleHandler {
         .build()
     );
 
+    /**
+     * Registers a job with the scheduler service, the job will define how often
+     * it should run, and the schedule service will then periodically run the
+     * job on a separate thread when it's time for it to run.
+     *
+     * @param job The job that should be registered with the scheduler service.
+     */
     public static void registerJob(@Nonnull Job job) {
         tasks.add(schedulerService.scheduleAtFixedRate(job, job.getDelay(), job.getPeriod(), job.getUnit()));
     }
 
+    /**
+     * Gets a set of scheduled future instances for jobs
+     * that are registered to the scheduler service.
+     *
+     * @return A set of scheduled future instances for registered jobs.
+     */
     public static Set<ScheduledFuture<?>> entrySet() {
         return tasks;
     }
 
+    /**
+     * Gets the scheduler execution service used to
+     * register the jobs with.
+     *
+     * @return The scheduler service used for all the registered jobs.
+     */
     public static ScheduledExecutorService getScheduler() {
         return schedulerService;
     }
