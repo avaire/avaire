4849949463a94abdd9036697b0b1eb91929c410a
Alexis Tan <alexis@sen-dev.com>
2019-10-29 17:15:09 +0100

    Add documentation to event handlers

--- src/main/java/com/avairebot/handlers/DatabaseEventHolder.java
+++ src/main/java/com/avairebot/handlers/DatabaseEventHolder.java
@@ -31,15 +31,38 @@ public class DatabaseEventHolder {
     private final GuildTransformer guild;
     private final PlayerTransformer player;
 
+    /**
+     * Creates a new database holder instance using
+     * the given guild and player transformer.
+     *
+     * @param guild  The guild transformer that should be stored in the event holder.
+     * @param player The player transformer that should be stored in the event holder.
+     */
     public DatabaseEventHolder(GuildTransformer guild, PlayerTransformer player) {
         this.guild = guild;
         this.player = player;
     }
 
+    /**
+     * Gets the guild database transformer for the current JDA event,
+     * the guild transformer can be used to pull specific bot server
+     * settings and information about the current guild.
+     *
+     * @return The guild database transformer for the current JDA event,
+     *         or {@code NULL} if the event was not invoked for a guild.
+     */
     public GuildTransformer getGuild() {
         return guild;
     }
 
+    /**
+     * Gets the player database transformer for the current JDA event,
+     * the player transformer can be used to pull player experience
+     * information about the user who invoked the JDA event.
+     *
+     * @return The player database transformer for the user who invoked the JDA event,
+     *         or {@code NULL} if the leveling feature is disabled in the guild.
+     */
     @Nullable
     public PlayerTransformer getPlayer() {
         return player;
--- src/main/java/com/avairebot/handlers/events/ApplicationShutdownEvent.java
+++ src/main/java/com/avairebot/handlers/events/ApplicationShutdownEvent.java
@@ -31,20 +31,41 @@ public class ApplicationShutdownEvent extends Event {
 
     private final ShutdownReason reason;
 
+    /**
+     * Creates a new application shutdown event, this is called just before
+     * the application is about to shutdown completely.
+     *
+     * @param api      The JDA api(shard) that the event should be triggered on.
+     * @param exitCode The exit code that the application is closing with.
+     */
     public ApplicationShutdownEvent(JDA api, int exitCode) {
         super(api);
 
         reason = ShutdownReason.fromExitCode(exitCode);
     }
 
+    /**
+     * Gets the reason for the shutdown.
+     *
+     * @return The reason for the shutdown.
+     */
     public ShutdownReason getReason() {
         return reason;
     }
 
+    /**
+     * Gets the exit code that the application will shutdown with.
+     *
+     * @return The exit code that the application will shutdown with.
+     */
     public int getExitCode() {
         return reason.getExitCode();
     }
 
+    /**
+     * The shutdown reasons available for the
+     * different supported exit codes.
+     */
     public enum ShutdownReason {
 
         NORMAL(ExitCodes.EXIT_CODE_NORMAL),
@@ -59,6 +80,13 @@ public class ApplicationShutdownEvent extends Event {
             this.exitCode = exitCode;
         }
 
+        /**
+         * Gets the shutdown reason from the given exit code.
+         *
+         * @param exitCode The exit code the shutdown reason should match with.
+         * @return The matching shutdown reason to the given exit code,
+         *         or {@link #UNKNOWN} if there were no match.
+         */
         @Nonnull
         public static ShutdownReason fromExitCode(int exitCode) {
             for (ShutdownReason reason : values()) {
@@ -69,6 +97,11 @@ public class ApplicationShutdownEvent extends Event {
             return ShutdownReason.UNKNOWN;
         }
 
+        /**
+         * Gets the shutdown reason exit code.
+         *
+         * @return The shutdown reason exit code.
+         */
         public int getExitCode() {
             return exitCode;
         }
