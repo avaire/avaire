096d3122b7da9324c30d19c9f254a49b94889210
Macley <26381427+Macley-Kun@users.noreply.github.com>
2019-11-03 23:41:46 +0100

    Update buildpack-run.sh to support plugins (#129)

--- buildpack-run.sh
+++ buildpack-run.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 if [ -d ".git" ]; then # After compile remove .git (as it will be replaced) and delete everything else with exception after $. Get website except wrong Procfile for this build
-  rm -r .git/ & rm -rf $(find * -name "*" ! -name "AvaIre.jar" ! -name "avairebotapache2.conf" ! -name "Procfile" ! -name "update.sh" ! -name "plugins/*")
+  mv plugins/ ../plugins/ # Save the plugins folder, below methode doesn't cover folders.
+  rm -r .git/ & rm -rf $(find * -name "*" ! -name "AvaIre.jar" ! -name "avairebotapache2.conf" ! -name "Procfile" ! -name "update.sh")
+  mv ../plugins/ . # Save the plugins folder placing it back
   git clone https://github.com/avaire/website && rm website/Procfile && mv website/{.[!.],}* . && rm -r website/ # Example: avaire/website
   java -jar AvaIre.jar --generate-json-file && mv commandMap.json storage/commandMap.json # Get needed configs and move command file to right directory
 else # If AvaIre.jar isn't compiled
