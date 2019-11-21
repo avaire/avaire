5c0cfa78a91f2efcc5090583c4aeeca0e51f25a1
Aaron <a.rcottrill521@gmail.com>
2019-11-21 17:10:16 -0500

    Create holiday command (#131)
    * Create a working holiday command
    
    Holiday information is cached ready for use all day the bot is used.
    
    * Remove my API key
    
    That was extremely dumb to leave it in there. Oops.
    
    * Extract the lamda expression
    
    It's now in a proper method.
    
    * Fix my key check
    
    This should fix my length issue.
    
    * Remove unused properties
    
    There the values should be a lot clearer now.
    
    * Added an extra line
    
    * Try to get my IDE to stop changing my code spacing.
    
    * Add the api key to app.json and uppercase the g in get.
    
    * Fix code formatting
    
    * Hide command when provided an invalid API token
    
    * Refactor command to prevent creating unnecessary objects and cleanup a bit
    
    * Restructor inherited methods to match the order every other command uses
    
    * Add more description and fix command name
    
    * Change command throttle scope to the channel from the user
    
    * Bump version

---
+++ src/main/java/com/avairebot/requests/service/HolidayService.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright (c) 2019.
+ *
+ * This file is part of AvaIre.
+ *
+ * AvaIre is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * AvaIre is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with AvaIre.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ *
+ */
+
+package com.avairebot.requests.service;
+
+import java.util.List;
+
+public class HolidayService {
+    
+    private List<Holiday> holidays;
+    private String error;
+
+    public List<Holiday> getHolidays() {
+        return holidays;
+    }
+
+    public String getError() {
+        return error;
+    }
+
+    public class Holiday {
+
+        private String name;
+        private String date;
+
+        private boolean isPublic;
+
+        public boolean isPublic() {
+            return isPublic;
+        }
+
+        public void setPublic(boolean aPublic) {
+            isPublic = aPublic;
+        }
+
+        public String getDate() {
+            return date;
+        }
+
+        public String getName() {
+            return name;
+        }
+    }
+}
