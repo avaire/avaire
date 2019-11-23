44549e0cfa50d2ddca41b9420f2ba3d124b7a879
Alexis Tan <alexis@sen-dev.com>
2019-11-23 17:25:56 +0100

    Uncomment Lavalink disconnect as it works with the new LL client

--- src/main/java/com/avairebot/scheduler/tasks/GarbageCollectorTask.java
+++ src/main/java/com/avairebot/scheduler/tasks/GarbageCollectorTask.java
@@ -36,6 +36,7 @@ import com.avairebot.handlers.adapter.JDAStateEventAdapter;
 import com.avairebot.handlers.adapter.MessageEventAdapter;
 import com.avairebot.scheduler.jobs.LavalinkGarbageNodeCollectorJob;
 import lavalink.client.io.Link;
+import lavalink.client.io.jda.JdaLink;
 import net.dv8tion.jda.api.managers.AudioManager;
 
 import java.util.Map;
@@ -110,15 +111,13 @@ public class GarbageCollectorTask implements Task {
         // If Lavalink is enabled we'll use the Lavalink link state
         // for the current guild instead of the audio manager.
         if (LavalinkManager.LavalinkManagerHolder.lavalink.isEnabled()) {
-            // TODO: FIx Lavalink Client working with JDA v4.
-//            JdaLink link = LavalinkManager.LavalinkManagerHolder.lavalink.getLavalink().getExistingLink(
-//                next.getValue().getLastActiveMessage().getGuild()
-//            );
+            JdaLink link = LavalinkManager.LavalinkManagerHolder.lavalink.getLavalink().getExistingLink(
+                next.getValue().getLastActiveMessage().getGuild()
+            );
 
-//            if (link != null && isConnected(link.getState())) {
-//                return false;
-//            }
-            return false;
+            if (link != null && isConnected(link.getState())) {
+                return false;
+            }
         } else {
             if (isConnected(next.getValue().getLastActiveMessage().getGuild().getAudioManager())) {
                 return false;
