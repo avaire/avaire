eb6c3c4d72bebfbc23f72fb709f5367edbc40149
Alexis Tan <alexis@sen-dev.com>
2020-04-08 15:28:13 +0200

    Fix code formatting + remove inherited docs

--- src/main/java/com/avairebot/commands/interaction/HandHoldCommand.java
+++ src/main/java/com/avairebot/commands/interaction/HandHoldCommand.java
@@ -22,7 +22,6 @@
 package com.avairebot.commands.interaction;
 
 import com.avairebot.AvaIre;
-import com.avairebot.contracts.commands.CommandContext;
 import com.avairebot.contracts.commands.InteractionCommand;
 
 import javax.annotation.Nonnull;
@@ -30,55 +29,30 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-public class HandHoldCommand extends InteractionCommand
-{
-    /**
-     * Creates a new interaction command instance.
-     *
-     * @param avaire The main {@link AvaIre avaire} application instance.
-     */
+public class HandHoldCommand extends InteractionCommand {
+
     public HandHoldCommand(AvaIre avaire) {
         super(avaire);
     }
 
-    /**
-     * Gets the list of interaction images that can be returned by the
-     * interaction, using the image full URL to where it is hosted.
-     * <p>
-     * Example: https://i.imgur.com/ZupgGkI.jpg
-     *
-     * @return A list of interaction images.
-     */
     @Nonnull
     @Override
-    public List<String> getInteractionImages()
-    {
-        return Arrays.asList("https://imgur.com/fk8RpWF.gif","https://imgur.com/4p4afJU.gif",
-                    "https://imgur.com/O9Zi3TB.gif","https://imgur.com/SHY2BPh.gif");
+    public List<String> getInteractionImages() {
+        return Arrays.asList(
+            "https://imgur.com/fk8RpWF.gif",
+            "https://imgur.com/4p4afJU.gif",
+            "https://imgur.com/O9Zi3TB.gif",
+            "https://imgur.com/SHY2BPh.gif"
+        );
     }
 
-    /**
-     * Gets the command name, this is used in help and error
-     * messages as the title as well as log messages.
-     *
-     * @return Never-null, the command name.
-     */
     @Override
-    public String getName()
-    {
+    public String getName() {
         return "Hand Hold Command";
     }
 
-    /**
-     * Gets am immutable list of command triggers that can be used to invoke the current
-     * command, the first index in the list will be used when the `:command` placeholder
-     * is used in {@link #getDescription(CommandContext)} or {@link #getUsageInstructions()} methods.
-     *
-     * @return An immutable list of command triggers that should invoked the command.
-     */
     @Override
-    public List<String> getTriggers()
-    {
+    public List<String> getTriggers() {
         return Collections.singletonList("handhold");
     }
 }
