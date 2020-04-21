da62717929418e210b46d2740e636ff84eaa67df
WittiJ <njwittig183@gmail.com>
2020-04-21 23:18:20 +0200

    t

--- src/main/java/com/avairebot/commands/utility/SourceCommand.java
+++ src/main/java/com/avairebot/commands/utility/SourceCommand.java
@@ -14,7 +14,7 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with AvaIre.  If not, see <https://www.gnu.org/licenses/>.
+ * along with AvaIre.  If not, see <https://www.gnu.org/licenses/>.t
  *
  *
  */
