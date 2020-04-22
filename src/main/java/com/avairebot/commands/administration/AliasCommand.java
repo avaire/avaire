60d5ce707b803f07dbcb0aacd5440267225039ea
WittiJ <njwittig183@gmail.com>
2020-04-22 14:34:54 +0200

    Update AliasCommand.java

--- src/main/java/com/avairebot/commands/administration/AliasCommand.java
+++ src/main/java/com/avairebot/commands/administration/AliasCommand.java
@@ -67,7 +67,7 @@ public class AliasCommand extends Command {
     public List<String> getExampleUsage(@Nullable Message message) {
         //noinspection ConstantConditions
         return Collections.singletonList(String.format(
-            "`:command !ava %srepeat **Website:** https://avairebot.com/`",
+            "`:command !ava %srepeat **Website:** http://sn0w.xyz/`",
             message == null
                 ? CommandHandler.getCommand(AliasCommand.class).getCategory().getPrefix()
                 : generateCommandPrefix(message)
