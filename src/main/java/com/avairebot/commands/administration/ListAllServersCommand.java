fed562ba8e9f9d639a5d541a8c7040101125a5b3
WittiJ <njwittig183@gmail.com>
2020-04-22 14:58:56 +0200

    Add files via upload

---
+++ src/main/java/com/avairebot/commands/administration/ListAllServersCommand.java
@@ -0,0 +1,106 @@
+/*
+ * Copyright (c) 2018.
+ *
+ * This file is part of AvaIre.
+ *
+ * AvaIre is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * AvaIre is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with AvaIre.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ *
+ */
+
+package com.avairebot.commands.administration;
+
+import com.avairebot.AvaIre;
+import com.avairebot.chat.SimplePaginator;
+import com.avairebot.commands.CommandHandler;
+import com.avairebot.commands.CommandMessage;
+import com.avairebot.contracts.commands.Command;
+import com.avairebot.contracts.commands.CommandGroup;
+import com.avairebot.contracts.commands.CommandGroups;
+import com.avairebot.database.collection.Collection;
+import com.avairebot.database.controllers.ReactionController;
+import com.avairebot.language.I18n;
+import com.avairebot.utilities.NumberUtil;
+import com.google.gson.reflect.TypeToken;
+import net.dv8tion.jda.core.entities.Emote;
+
+import javax.annotation.Nonnull;
+import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class ListAllServersCommand extends Command {
+
+    public ListAllServersCommand(AvaIre avaire) {
+        super(avaire, false);
+    }
+
+    @Override
+    public String getName() {
+        return "List All Servers Command";
+    }
+
+    @Override
+    public String getDescription() {
+        return "List all the Servers the bot is on.";
+    }
+
+    @Override
+    public List<String> getUsageInstructions() {
+        return Collections.singletonList(
+            "`:command [page]` - Lists all servers."
+        );
+    }
+
+    @Override
+    public List<Class<? extends Command>> getRelations() {
+        return Arrays.asList(
+            AddReactionRoleCommand.class,
+            RemoveReactionRoleCommand.class
+        );
+    }
+
+    @Override
+    public List<String> getTriggers() {
+        return Collections.singletonList("lrr");
+    }
+
+    @Override
+    public List<String> getMiddleware() {
+        return Arrays.asList(
+            "require:user,general.administrator",
+            "throttle:guild,1,5"
+        );
+    }
+
+    @Nonnull
+    @Override
+    public List<CommandGroup> getGroups() {
+        return Collections.singletonList(
+            CommandGroups.ROLE_ASSIGNMENTS
+        );
+    }
+
+    @Override
+    @SuppressWarnings("ConstantConditions")
+    public boolean onCommand(CommandMessage context, String[] args) {
+        Collection collection = avaire.GetGuilds();
+		for (String guild : collection) {
+			members = guild.getMembers();
+			if (members < 4) {
+			 Console.WriteLine("Name: {0} and ID: {1}", guild.name, guild.id);
+			}
+         }
+        return false;
+    }
+}
