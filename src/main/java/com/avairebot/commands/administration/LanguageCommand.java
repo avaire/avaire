ee21360695b5d926c57864238c193d05ae2b8a9a
Avaire-Translations <70543419+Avaire-Translations@users.noreply.github.com>
2020-09-02 22:36:06 +0200

    Updated language command
    Added disclaimer for translation interface.

--- src/main/java/com/avairebot/commands/administration/LanguageCommand.java
+++ src/main/java/com/avairebot/commands/administration/LanguageCommand.java
@@ -52,7 +52,8 @@ public class LanguageCommand extends Command {
 
     @Override
     public String getDescription() {
-        return "Show a list of available languages or set a language that should be used for the server.";
+        return "Show a list of available languages or set a language that should be used for the server.\n" + 
+            "If you are interested to contribute to translations for AvaIre, then head on over to [this](https://translate.avairebot.com/) website to translate AvaIre!";
     }
 
     @Override
